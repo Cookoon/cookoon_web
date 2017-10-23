58def1d905881b1e88ddeb130eb7291660c81844
Charles Pernet <charlespernet@gmail.com>
2017-10-23 11:59:22 +0200

    Add Reservation Policy #update? #show?

--- app/policies/reservation_policy.rb
+++ app/policies/reservation_policy.rb
@@ -1,7 +1,15 @@
 class ReservationPolicy < ApplicationPolicy
+  def show?
+    record.user == user
+  end
+
   def create?
     #can be updated to check if user has a credit card
     true
+  end
+
+  def update?
+    record.user == user
   end
 
   class Scope < Scope
