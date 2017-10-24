aa1081aef79df9c7e1c075df0773f09fd94341dd
Charles Pernet <charlespernet@gmail.com>
2017-10-24 12:29:34 +0200

    Change #create and #update in InventoriesController

--- app/controllers/host/inventories_controller.rb
+++ app/controllers/host/inventories_controller.rb
@@ -8,8 +8,7 @@ class Host::InventoriesController < ApplicationController
   end
 
   def create
-    full_params = checkin_inventory_params.merge(checkin_at: DateTime.now)
-    @inventory = @reservation.build_inventory(full_params)
+    @inventory = @reservation.build_inventory(checkin_at: DateTime.now)
     if @inventory.save && @reservation.ongoing!
       paiement_service = StripePaiementService.new(user: @reservation.cookoon_owner, reservation: @reservation)
       # Pas de test ici, il faut monitorer sur les premieres locations
@@ -29,7 +28,7 @@ class Host::InventoriesController < ApplicationController
     reservation = @inventory.reservation
     full_params = checkout_inventory_params.merge({checkout_at: DateTime.now, status: :checked_out})
     if @inventory.update(full_params) && reservation.passed!
-      flash[:noice] = "La réservation est maintenant terminée"
+      flash[:notice] = "La réservation est maintenant terminée"
       redirect_to host_reservations_path
     else
       render :edit
@@ -58,11 +57,7 @@ class Host::InventoriesController < ApplicationController
     authorize [:host, @reservation]
   end
 
-  def checkin_inventory_params
-    params.require(:inventory).permit(checkin_photos: [])
-  end
-
   def checkout_inventory_params
-    params.require(:inventory).permit(checkout_photos: [])
+    params.require(:inventory).permit(:remark)
   end
 end
