054d9dba55866b9b2272fcf16a61bbbe73e5aaec
Charles Pernet <charlespernet@gmail.com>
2017-11-09 10:04:53 +0100

    Add Reservation mailer

---
+++ app/mailers/reservation_mailer.rb
@@ -0,0 +1,61 @@
+class ReservationMailer < ApplicationMailer
+  # ==== Mails for Users =====
+  def new_request(reservation)
+    @reservation = reservation
+    @tenant = @reservation.user
+    @cookoon = @reservation.cookoon
+    @host = @cookoon.user
+    mail(to: @tenant.email, subject: 'Vous avez demandé un Cookoon !')
+  end
+
+  def confirmed_by_host(reservation)
+    @reservation = reservation
+    @tenant = @reservation.user
+    @cookoon = @reservation.cookoon
+    @host = @cookoon.user
+    mail(to: @tenant.email, subject: "Votre réservation Cookoon est confirmée : #{@cookoon.name}")
+  end
+
+  def refused_by_host(reservation)
+    @reservation = reservation
+    @tenant = @reservation.user
+    @cookoon = @reservation.cookoon
+    @host = @cookoon.user
+    mail(to: @tenant.email, subject: "Votre réservation Cookoon a été refusée : #{@cookoon.name}")
+  end
+
+  def ending_survey_for_user(reservation)
+    @reservation = reservation
+    @tenant = @reservation.user
+    @cookoon = @reservation.cookoon
+    @host = @cookoon.user
+    mail(to: @tenant.email, subject: "Comment s’est passée votre expérience Cookoon ?")
+  end
+  # ============================
+
+  # ==== Mails for Host =====
+  def pending_request(reservation)
+    @reservation = reservation
+    @tenant = @reservation.user
+    @cookoon = @reservation.cookoon
+    @host = @cookoon.user
+    mail(to: @host.email, subject: "#{@tenant.first_name} souhaite louer votre Cookoon !")
+  end
+
+  def confirmation(reservation)
+    @reservation = reservation
+    @tenant = @reservation.user
+    @cookoon = @reservation.cookoon
+    @host = @cookoon.user
+    mail(to: @host.email, subject: "Vous avez confirmé une demande de location !")
+  end
+
+  def ending_survey_for_host(reservation)
+    @reservation = reservation
+    @tenant = @reservation.user
+    @cookoon = @reservation.cookoon
+    @host = @cookoon.user
+    mail(to: @tenant.email, subject: "Comment s’est passée votre expérience Cookoon ?")
+  end
+  # ============================
+end
