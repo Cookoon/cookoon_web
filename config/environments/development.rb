25a7581f32c1b17ba29c78920121bc5221a2e9a4
Charles Pernet <charlespernet@gmail.com>
2017-11-09 10:06:04 +0100

    Change preview emails path

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,6 +35,8 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :letter_opener
 
+  config.action_mailer.preview_path = 'test/mailers/previews'
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
