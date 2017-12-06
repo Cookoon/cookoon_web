a60c6e739b27333e66660d478eecac29e03baf6d
Charles Pernet <charlespernet@gmail.com>
2017-12-06 16:36:07 +0100

    Fix fees for tenant in JS computing

--- app/assets/javascripts/price_computing.js
+++ app/assets/javascripts/price_computing.js
@@ -6,7 +6,7 @@ document.addEventListener("turbolinks:load", function() {
   $('#reservation_duration').change(function() {
     compute_price_for_rent($(this));
   });
-})  
+})
 
 function compute_price_for_host(input) {
   var displayPrice = parseFloat($('#display-price').text());
@@ -21,6 +21,6 @@ function compute_price_for_host(input) {
 
 function compute_price_for_rent(duration_input) {
   var price_cents_without_fees = ($('#cookoon-price').text() * duration_input.val()) * 100;
-  var total_price_cents = (price_cents_without_fees * 1.07)
+  var total_price_cents = (price_cents_without_fees * 1.05)
   $('#total-price').text(+(total_price_cents / 100).toFixed(2));
 }
