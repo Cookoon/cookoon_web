80ebce539fcdba9f0e7a723d442f8fb0b42c983e
François Catuhe <fcatuhe@gmail.com>
2017-12-06 16:07:39 +0100

    finish reservation modal ajaxification, improve javascript to be triggered on turbolinks:load and ajaxComplete, centralize datepicker in javascript file

--- app/assets/javascripts/price_computing.js
+++ app/assets/javascripts/price_computing.js
@@ -1,4 +1,4 @@
-document.addEventListener("turbolinks:load", function() {
+$(document).on('turbolinks:load ajaxComplete', function() {
   $('.reservation-option').change(function() {
     compute_price_for_host($(this));
   });
@@ -6,21 +6,21 @@ document.addEventListener("turbolinks:load", function() {
   $('#reservation_duration').change(function() {
     compute_price_for_rent($(this));
   });
-})  
+});
 
 function compute_price_for_host(input) {
   var displayPrice = parseFloat($('#display-price').text());
   var optionPrice = input.data('price');
-  if(input.is(":checked")) {
+  if (input.is(':checked')) {
     $('#display-price').text(displayPrice - optionPrice);
-  }
-  else {
+  } else {
     $('#display-price').text(displayPrice + optionPrice);
   }
 }
 
 function compute_price_for_rent(duration_input) {
-  var price_cents_without_fees = ($('#cookoon-price').text() * duration_input.val()) * 100;
-  var total_price_cents = (price_cents_without_fees * 1.07)
+  var price_cents_without_fees =
+    $('#cookoon-price').text() * duration_input.val() * 100;
+  var total_price_cents = price_cents_without_fees * 1.07;
   $('#total-price').text(+(total_price_cents / 100).toFixed(2));
 }
