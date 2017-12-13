226cd168caad66c2e4df80f4d560144afb41b100
François Catuhe <fcatuhe@gmail.com>
2017-12-13 11:06:49 +0100

    replace paiement_service with payment_service

--- app/controllers/host/inventories_controller.rb
+++ app/controllers/host/inventories_controller.rb
@@ -10,10 +10,10 @@ class Host::InventoriesController < ApplicationController
   def create
     @inventory.attributes = inventory_params.merge(checkin_at: DateTime.now)
     if @inventory.save && @reservation.ongoing!
-      paiement_service = StripePaymentService.new(user: @reservation.cookoon_owner, reservation: @reservation)
+      payment_service = StripePaymentService.new(user: @reservation.cookoon_owner, reservation: @reservation)
       # Pas de test ici, il faut monitorer sur les premieres locations
       # Ajouter une transaction ? Ou poster sur Slack pour le declencher à la main.
-      paiement_service.tax_and_payout
+      payment_service.tax_and_payout
       flash[:notice] = 'La reservation vient de démarrer'
       redirect_to host_reservations_path
     else
