543b653147159643fe3bf6cb7011f80b893bc540
Charles Pernet <charlespernet@gmail.com>
2017-12-14 16:38:21 +0100

    Update UsersPolicy to work with pretender gem

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -3,9 +3,24 @@ class UserPolicy < ApplicationPolicy
     user == record
   end
 
+  def impersonate?
+    user_can_access?
+  end
+
+  def stop_impersonating?
+    user_can_access?
+  end
+
+  private
+
+  def user_can_access?
+    # we need to perform check on record because of pretender gem
+    record.admin?
+  end
+
   class Scope < Scope
     def resolve
-      scope
+      scope.where.not(admin: true)
     end
   end
 end
