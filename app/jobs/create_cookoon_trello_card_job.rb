a05ab02d5512bef0c25bd67b065fb3c167ea7468
Charles Pernet <charlespernet@gmail.com>
2017-12-18 12:44:17 +0100

    Fix Typo in jobs

--- app/jobs/create_cookoon_trello_card_job.rb
+++ app/jobs/create_cookoon_trello_card_job.rb
@@ -2,7 +2,7 @@ class CreateCookoonTrelloCardJob < ApplicationJob
   queue_as :default
 
   def perform(cookoon_id)
-    cookoon = Cookon.find(cookoon_id)
+    cookoon = Cookoon.find(cookoon_id)
     service = TrelloCookoonService.new(cookoon: cookoon)
     service.create_trello_card
   end
