80befe6518032c3d949015d94b245b8e2bf76fae
Charles Pernet <charlespernet@gmail.com>
2017-12-27 12:14:35 +0100

    Done with InvoiceMailer

--- app/mailers/invoice_mailer.rb
+++ app/mailers/invoice_mailer.rb
@@ -1,4 +1,7 @@
 class InvoiceMailer < ApplicationMailer
   def new_request(reservation)
+    @reservation = reservation
+    @tenant = @reservation.user
+    mail(to: 'concierge@cookoon.fr', subject: 'Nouvelle demande de Facture') { |format| format.text }
   end
 end
