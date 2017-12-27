f924c715ae1586f42714c8764b6dabc217e3befc
Charles Pernet <charlespernet@gmail.com>
2017-12-27 12:00:06 +0100

    Done with InvoicesController

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,7 +1,10 @@
 class InvoicesController < ApplicationController
   skip_after_action :verify_authorized
-  
+
   def create
     @reservation = Reservation.find(params[:reservation_id])
+    InvoiceMailer.new_request(@reservation).deliver_later
+    flash[:notice] = 'Vous recevrez votre facture par e-mail très prochainement'
+    redirect_to reservations_path
   end
 end
