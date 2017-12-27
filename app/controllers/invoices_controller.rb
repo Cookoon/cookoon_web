d795a5f82edb515a2a6bd183f1e100b9f10898fa
Charles Pernet <charlespernet@gmail.com>
2017-12-27 11:53:18 +0100

    Add actual link on Reservation Show to ask for Invoice

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,5 +1,7 @@
 class InvoicesController < ApplicationController
+  skip_after_action :verify_authorized
+  
   def create
-    
+    @reservation = Reservation.find(params[:reservation_id])
   end
 end
