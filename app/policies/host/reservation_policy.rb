1e8cf4fbc54252afcc2a2027be70d14c28a7376c
Charles Pernet <charlespernet@gmail.com>
2017-12-28 09:35:47 +0100

    Remove .displayable scope from Host::ReservationPolicy

--- app/policies/host/reservation_policy.rb
+++ app/policies/host/reservation_policy.rb
@@ -10,7 +10,7 @@ class Host::ReservationPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
       # here scope is an array because of namespacing
-      scope.last.for_host(user).displayable
+      scope.last.for_host(user)
     end
   end
 end
