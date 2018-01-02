02d242820c76b47a88b1d538d427dbdd86986c49
Francois Catuhe <fcatuhe@gmail.com>
2018-01-02 21:57:30 +0100

    forestadmin bug on smart relationship

--- lib/forest_liana/collections/user.rb
+++ lib/forest_liana/collections/user.rb
@@ -5,7 +5,15 @@ module Forest
     collection 'User'
 
     field :full_name, type: 'String' do
-      "#{object.first_name} #{object.last_name}"
+      object.full_name
+    end
+    #
+    # field :invited_by_full_name, type: 'String' do
+    #   object.class.find(object.invited_by_id).full_name
+    # end
+
+    belongs_to :invited_by, reference: 'users.full_name' do
+      object.class.find(object.invited_by_id)
     end
   end
 end
