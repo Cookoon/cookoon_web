c94e9782ca7b795891c3bf521c722438e21dabc8
Francois Catuhe <fcatuhe@gmail.com>
2018-01-02 21:59:32 +0100

    add invited_by_full_name in forestadmin

--- lib/forest_liana/collections/user.rb
+++ lib/forest_liana/collections/user.rb
@@ -7,13 +7,9 @@ module Forest
     field :full_name, type: 'String' do
       object.full_name
     end
-    #
-    # field :invited_by_full_name, type: 'String' do
-    #   object.class.find(object.invited_by_id).full_name
-    # end
 
-    belongs_to :invited_by, reference: 'users.full_name' do
-      object.class.find(object.invited_by_id)
+    field :invited_by_full_name, type: 'String' do
+      object.class.find(object.invited_by_id).full_name
     end
   end
 end
