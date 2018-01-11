919af39af03148233c7d8762e67e00894c1e0531
Francois Catuhe <fcatuhe@gmail.com>
2018-01-11 18:14:08 +0100

    replace date helper with synonym

--- app/jobs/notify_reservation_in_three_hours_job.rb
+++ app/jobs/notify_reservation_in_three_hours_job.rb
@@ -3,7 +3,7 @@ class NotifyReservationInThreeHoursJob < ApplicationJob
 
   def perform(scheduled_time)
     scheduled_date_time = Time.zone.at(scheduled_time)
-    Reservation.in_hour_range_around(scheduled_date_time.since(3.hours)).each do |reservation|
+    Reservation.in_hour_range_around(scheduled_date_time.in(3.hours)).each do |reservation|
       ReservationMailer.notify_tenant_before_reservation(reservation).deliver_later
     end
   end
