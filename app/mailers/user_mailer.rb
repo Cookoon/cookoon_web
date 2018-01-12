5482c4bd5c58b5f7f407bbd4337982eaf187ea0d
Francois Catuhe <fcatuhe@gmail.com>
2018-01-12 18:34:51 +0100

    implement notify_invitations_awarded mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,4 +3,10 @@ class UserMailer < ApplicationMailer
     @user = user
     mail(to: @user.full_email, subject: 'Bienvenue dans la communauté Cookoon !')
   end
+
+  def notify_invitations_awarded(user, invitation_quantity)
+    @user = user
+    @invitation_quantity = invitation_quantity
+    mail(to: @user.full_email, subject: "Nous venons de vous offrir #{invitation_quantity} invitations")
+  end
 end
--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -2,4 +2,8 @@ class UserMailerPreview < ActionMailer::Preview
   def welcome_email
     UserMailer.welcome_email(User.last)
   end
+
+  def notify_invitations_awarded
+    UserMailer.notify_invitations_awarded(User.last, 5)
+  end
 end
