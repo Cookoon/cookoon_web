0f9be7ed76de9332f07b61f62c9c00396b8a20e2
Francois Catuhe <fcatuhe@gmail.com>
2018-01-29 12:27:12 +0100

    remote static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,40 +1,23 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, except: [:support]
-  layout "naked", except: [:support, :cgu]
+  skip_before_action :authenticate_user!, except: :support
+  layout 'naked', except: :support
 
-  def home
-  end
+  def home; end
 
-  def about
-  end
-
-  def about_rent
-  end
-
-  def about_hosting
-  end
-
-  def about_warranties
-  end
-
-  def support
-  end
-
-  def cgu
-  end
+  def support; end
 
   def setcookies
     lat_lng = "#{params[:lat]}|#{params[:lng]}"
-    cookies[:lat_lng] ||= { value: lat_lng, expires: 5.hour.from_now }
+    cookies[:lat_lng] ||= { value: lat_lng, expires: 5.hours.from_now }
   end
 
   def apple_app_site_association
-    association_json = File.read(Rails.public_path + "apple-app-site-association")
-    render json: association_json, content_type: "application/json"
+    association_json = File.read(Rails.public_path + 'apple-app-site-association')
+    render json: association_json, content_type: 'application/json'
   end
 
   def android_assetlinks
-    association_json = File.read(Rails.public_path + "android_assetlinks.json")
-    render json: association_json, content_type: "application/json"
+    association_json = File.read(Rails.public_path + 'android_assetlinks.json')
+    render json: association_json, content_type: 'application/json'
   end
 end
