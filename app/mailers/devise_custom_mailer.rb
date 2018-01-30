f05b84f9cbbf16d4cc6e1b6d50c3e0899fb05a82
Francois Catuhe <fcatuhe@gmail.com>
2018-01-30 18:24:39 +0100

    Use I18n in devise custom mailer, lint

--- app/mailers/devise_custom_mailer.rb
+++ app/mailers/devise_custom_mailer.rb
@@ -3,8 +3,11 @@ class DeviseCustomMailer < Devise::Mailer
   include Devise::Controllers::UrlHelpers
   default template_path: 'devise/mailer'
 
-  def invitation_instructions(record, token, opts={})
-    opts[:subject] = "#{record&.invited_by.full_name} vous a invité a rejoindre Cookoon !"
+  def invitation_instructions(record, token, opts = {})
+    opts[:subject] = t(
+      'devise.mailer.invitation_instructions.subject',
+      inviter: record&.invited_by&.full_name
+    )
     super
   end
 end
