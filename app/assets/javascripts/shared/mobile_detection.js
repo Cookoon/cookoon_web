1ee953598f81bde8d75cffceaf4c9eec542db617
Francois Catuhe <fcatuhe@gmail.com>
2018-02-01 14:42:42 +0100

    refactor mobile_detection js

--- app/assets/javascripts/shared/mobile_detection.js
+++ app/assets/javascripts/shared/mobile_detection.js
@@ -1,28 +1,34 @@
 $(document).on('turbolinks:load', function() {
-  if ($('.pages.home').length || $('.invitations.edit')) {
-    const isIosBrowser = /iP(?:hone|od|ad).*AppleWebKit(?:.*Version)|(?:.*CriOS)/i.test(
-      navigator.userAgent
-    );
-    const isAndroidBrowser = /Android(?!.*; wv)/i.test(navigator.userAgent);
+  if ($('.pages.home').length || $('.invitations.edit').length) {
+    let mobileBrowser = null;
 
-    if (isIosBrowser) {
-      if (
-        window.confirm(
-          "L'application Cookoon est disponible pour votre mobile : souhaitez-vous l'installer ?"
-        )
-      ) {
-        window.location.href =
-          'https://itunes.apple.com/fr/app/cookoon-inside/id1291943406?mt=8';
-      }
-    } else if (isAndroidBrowser) {
-      if (
-        window.confirm(
-          "L'application Cookoon est disponible pour votre mobile : souhaitez-vous l'installer ?"
-        )
-      ) {
-        window.location.href =
-          'https://play.google.com/store/apps/details?id=cookoon.cookoonandroid';
-      }
+    if (
+      /iP(?:hone|od|ad).*AppleWebKit(?:.*Version)|(?:.*CriOS)/i.test(
+        navigator.userAgent
+      )
+    ) {
+      mobileBrowser = 'ios';
+    } else if (/Android(?!.*; wv)/i.test(navigator.userAgent)) {
+      mobileBrowser = 'android';
+    }
+
+    if (mobileBrowser) {
+      displayStoreMessage(mobileBrowser);
     }
   }
 });
+
+function displayStoreMessage(mobileBrowser) {
+  const storeUrl = {
+    ios: 'https://itunes.apple.com/fr/app/cookoon-inside/id1291943406?mt=8',
+    android:
+      'https://play.google.com/store/apps/details?id=cookoon.cookoonandroid'
+  };
+  if (
+    window.confirm(
+      "L'application Cookoon est disponible pour votre mobile : souhaitez-vous l'installer ?"
+    )
+  ) {
+    window.location.href = storeUrl[mobileBrowser];
+  }
+}
