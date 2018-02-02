9d552bcf06e3068371845573c67c16b3403ff163
Francois Catuhe <fcatuhe@gmail.com>
2018-02-02 10:44:30 +0100

    refactor user search form with stimulus

--- app/assets/javascripts/shared/search_block.js
+++ app/assets/javascripts/shared/search_block.js
@@ -5,18 +5,6 @@ $(document).on('turbolinks:load ajaxComplete', function() {
     $('.search-body').slideToggle();
   });
 
-  // Fill header on change
-  $('#user_search_number').change(function() {
-    $('#infos-number').text($(this).val());
-  });
-
-  // Listen click on control
-  $('.control').click(function() {
-    var clickedElem = $(this);
-    bumpElement(clickedElem);
-    modifyValue(clickedElem);
-  });
-
   // Address search clear button
   $('#user_search_address').keyup(function() {
     $('#user_search_address_clear').toggle(Boolean($(this).val()));
@@ -32,35 +20,3 @@ $(document).on('turbolinks:load ajaxComplete', function() {
     $(this).hide();
   });
 });
-
-function modifyValue(clickedElem) {
-  var $input = $(clickedElem.parent().data('input'));
-  var $field = $(clickedElem.parent().data('field'));
-  var action = clickedElem.data('action');
-  var min = $input.data('min');
-  var max = $input.data('max');
-  var value = parseInt($field.text(), 10);
-
-  if (action === 'minus') {
-    if ($input.val() > min) {
-      value -= 1;
-      $field.text(value);
-      $input.val(value);
-    }
-  } else if (action === 'plus') {
-    if ($input.val() < max) {
-      value += 1;
-      $field.text(value);
-      $input.val(value);
-    }
-  }
-  $input.trigger('change');
-}
-
-// Make buttons bump
-function bumpElement(clickedElem) {
-  clickedElem.addClass('bump');
-  setTimeout(function() {
-    clickedElem.removeClass('bump');
-  }, 400);
-}
--- app/javascript/controllers/user_searches_controller.js
+++ app/javascript/controllers/user_searches_controller.js
@@ -1,9 +1,78 @@
 import { Controller } from 'stimulus';
 
 export default class extends Controller {
-  static targets = ['date', 'dateInput'];
+  static targets = [
+    'date',
+    'dateInput',
+    'duration',
+    'durationInput',
+    'people',
+    'peopleInput'
+  ];
 
   updateDateTarget() {
     this.dateTarget.innerHTML = this.dateInputTarget.value;
+  }
+
+  decrementDuration(event) {
+    this.updateNumberAttribute({
+      updateValue: -1,
+      min: this.data.get('minDuration'),
+      input: this.durationInputTarget,
+      targets: this.durationTargets,
+      event
+    });
+  }
+
+  incrementDuration(event) {
+    this.updateNumberAttribute({
+      max: this.data.get('maxDuration'),
+      input: this.durationInputTarget,
+      targets: this.durationTargets,
+      event
+    });
+  }
+
+  decrementPeople(event) {
+    this.updateNumberAttribute({
+      updateValue: -1,
+      min: this.data.get('minPeople'),
+      input: this.peopleInputTarget,
+      targets: this.peopleTargets,
+      event
+    });
+  }
+
+  incrementPeople(event) {
+    this.updateNumberAttribute({
+      max: this.data.get('maxPeople'),
+      input: this.peopleInputTarget,
+      targets: this.peopleTargets,
+      event
+    });
+  }
+
+  updateNumberAttribute(params) {
+    params.updateValue = params.updateValue || 1;
+    params.inputValue = parseInt(params.input.value);
+
+    if (
+      (!params.min && !params.max) ||
+      (params.min && params.inputValue > parseInt(params.min)) ||
+      (params.max && params.inputValue < parseInt(params.max))
+    ) {
+      params.input.value = params.inputValue + params.updateValue;
+      params.targets.forEach(function(target) {
+        target.innerHTML = params.input.value;
+      });
+      this.bump(params.event.currentTarget);
+    }
+  }
+
+  bump(element) {
+    element.classList.add('bump');
+    setTimeout(function() {
+      element.classList.remove('bump');
+    }, 400);
   }
 }
