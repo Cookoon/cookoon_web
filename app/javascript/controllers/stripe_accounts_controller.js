80e1e64ccff790f211506a4fe9dae781706b8cc2
Francois Catuhe <fcatuhe@gmail.com>
2018-02-08 18:33:40 +0100

    refactor stripe accounts with Stimulus

---
+++ app/javascript/controllers/stripe_accounts_controller.js
@@ -0,0 +1,57 @@
+import { Controller } from 'stimulus';
+import Rails from 'rails-ujs';
+
+export default class extends Controller {
+  static targets = ['accountError', 'form', 'tokenInput'];
+
+  stripe = Stripe('pk_test_FSVbYboTFsYGkHZKX3jMm9sQ');
+
+  connect() {
+    if (this.hasFormTarget) {
+      this.formTarget.addEventListener('submit', this.handleSubmit);
+    } // TODO: FC 08feb18 configure Turbolinks cache to remove this?
+  }
+
+  disconnect() {
+    if (this.hasFormTarget) {
+      this.formTarget.removeEventListener('submit', this.handleSubmit);
+    } // TODO: FC 08feb18 configure Turbolinks cache to remove this?
+  }
+
+  handleSubmit = async event => {
+    event.preventDefault();
+    event.stopPropagation();
+
+    const { token, error } = await this.stripe.createToken('account', {
+      legal_entity: {
+        first_name: document.querySelector('.inp-first-name').value,
+        last_name: document.querySelector('.inp-last-name').value,
+        type: 'individual',
+        address: {
+          line1: document.querySelector('.inp-street-address1').value,
+          city: document.querySelector('.inp-city').value,
+          postal_code: document.querySelector('.inp-zip').value
+        },
+        dob: {
+          day: parseInt(document.querySelector('#stripe_dob_3i').value),
+          month: parseInt(document.querySelector('#stripe_dob_2i').value),
+          year: parseInt(document.querySelector('#stripe_dob_1i').value)
+        }
+      },
+      tos_shown_and_accepted: true
+    });
+
+    if (error) {
+      this.accountErrorTarget.textContent = error.message;
+    } else {
+      this.handleStripeToken(token);
+    }
+  };
+
+  handleStripeToken = token => {
+    this.tokenInputTarget.value = token.id;
+
+    this.formTarget.removeEventListener('submit', this.handleSubmit);
+    Rails.fire(this.formTarget, 'submit');
+  };
+}
