fe7882cf18997332480c102903e242d80de71193
Charles Pernet <charlespernet@gmail.com>
2018-02-14 15:13:54 +0100

    Add discount action in paymentsController to compute discount via XHR

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -13,7 +13,8 @@ class PaymentsController < ApplicationController
     payment_service = StripePaymentService.new(
       user: @reservation.user,
       source: params[:payment][:source],
-      reservation: @reservation
+      reservation: @reservation,
+      use_discount: params[:payment][:use_discount]
     )
     @user_cards = payment_service.user_sources.try(:data)
     if payment_service.create_charge_and_update_reservation
@@ -26,7 +27,32 @@ class PaymentsController < ApplicationController
     end
   end
 
+  def discount
+    base_price = @reservation.price_for_rent_with_fees
+    user_discount = @reservation.user.discount_balance
+    @computed_prices = compute_discount(base_price, user_discount)
+  end
+
   private
+
+  def compute_discount(base_price, user_discount)
+    if user_discount <= 0
+      {
+        discounted_price: base_price,
+        new_available_discount: user_discount
+      }
+    elsif base_price > user_discount
+      {
+        discounted_price: base_price - user_discount,
+        new_available_discount: Money.new(0, 'EUR')
+      }
+    else
+      {
+        discounted_price: Money.new(0, 'EUR'),
+        new_available_discount: user_discount - base_price
+      }
+    end
+  end
 
   def set_reservation
     @reservation = Reservation.where(status: :pending).find(params[:reservation_id])
