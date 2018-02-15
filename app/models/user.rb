2dcaf248cc1d6d2d5313de9c39e00d304458a79b
Charles Pernet <charlespernet@gmail.com>
2018-02-15 10:49:16 +0100

    Lint in User

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
   scope :with_reservation_finished_in_day_range_around, ->(date_time) { joins(:reservations).merge(Reservation.finished_in_day_range_around(date_time)).distinct }
   scope :has_cookoon, -> { joins(:cookoons).distinct }
   scope :has_no_cookoon, -> { left_outer_joins(:cookoons).where(cookoons: {id: nil}) }
-  scope :discount_expired, -> { where("discount_expires_at < ?", Time.zone.now) }
+  scope :discount_expired, -> { where('discount_expires_at < ?', Time.zone.now) }
   enum emailing_preferences: { no_emails: 0, all_emails: 1 }
 
   PHONE_REGEXP = /\A(\+\d+)?([\s\-\.]?\(?\d+\)?)+\z/
