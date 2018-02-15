9733b0ac35b9a26a9fdaaad2f1ddc38cc8ca5958
Francois Catuhe <fcatuhe@gmail.com>
2018-02-15 10:27:30 +0100

    improve order with case insensitivity

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -13,8 +13,8 @@ class Reservation < ApplicationRecord
   belongs_to :cookoon
   belongs_to :user
   has_one :inventory, dependent: :destroy
-  has_many :reservation_guests, -> { order('guests.last_name ASC') }, inverse_of: :reservation, dependent: :destroy
-  has_many :guests, -> { order(last_name: :asc) }, through: :reservation_guests
+  has_many :reservation_guests, -> { order('LOWER(guests.last_name) ASC').references(:guests) }, inverse_of: :reservation, dependent: :destroy
+  has_many :guests, through: :reservation_guests
 
   accepts_nested_attributes_for :guests
 
