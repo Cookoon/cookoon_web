0feba211bce304d255004e6bebb5a5c07f4d420b
Francois Catuhe <fcatuhe@gmail.com>
2018-02-15 16:52:35 +0100

    notify guest

--- app/models/reservation_guest.rb
+++ app/models/reservation_guest.rb
@@ -3,4 +3,12 @@ class ReservationGuest < ApplicationRecord
   belongs_to :guest
 
   validates :guest, uniqueness: { scope: :reservation }
+
+  after_create :notify_guest
+
+  private
+
+  def notify_guest
+    ReservationMailer.invited_by_tenant(reservation, guest).deliver_later
+  end
 end
