ebcd70a84c3a7b14bcb04fae5f612b1d024ace64
Charles Pernet <charlespernet@gmail.com>
2018-02-28 17:07:51 +0100

    Add concern for building availabilities page

--- app/controllers/availabilities_controller.rb
+++ app/controllers/availabilities_controller.rb
@@ -1,5 +1,6 @@
 class AvailabilitiesController < ApplicationController
-  include DatetimeHelper
+  include AvailabilitiesBuilder
+
   skip_after_action :verify_policy_scoped
   before_action :find_cookoon, only: %i[index create]
 
@@ -30,53 +31,5 @@ class AvailabilitiesController < ApplicationController
 
   def availability_params
     params.require(:availability).permit(:date, :time_slot)
-  end
-
-  def build_weeks(nb_of_weeks = 1)
-    weeks = []
-    nb_of_weeks.times do |i|
-      weeks << build_week(Time.zone.today + i.weeks)
-    end
-    weeks
-  end
-
-  def build_week(day_of_week)
-    first_day = day_of_week.beginning_of_week
-    last_day = day_of_week.end_of_week
-
-    {
-      display: "Semaine du #{build_display_week(first_day, last_day)}",
-      days: build_days(first_day, last_day)
-    }
-  end
-
-  def build_display_week(first_day, last_day)
-    [first_day, last_day].map do |day|
-      display_date_for(day, without_year: true)
-    end.join(' au ')
-  end
-
-  def build_days(first_day, last_day)
-    (first_day..last_day).to_a.map do |day|
-      {
-        display: display_date_for(day, without_year: true, with_weekda: true).capitalize,
-        time_slots: build_time_slots(day)
-      }
-    end
-  end
-
-  def build_time_slots(day)
-    Availability.time_slots.keys.map do |time_slot|
-      build_time_slot(date: day, time_slot: time_slot)
-    end
-  end
-
-  def build_time_slot(attrs = {})
-    availability = @availability || @cookoon.availabilities.find_by(attrs)
-    if availability
-      { url: availability_path(availability), method: :patch, available: availability.available }
-    else
-      { url: cookoon_availabilities_path(@cookoon), method: :post, available: true }
-    end.merge(attrs)
   end
 end
