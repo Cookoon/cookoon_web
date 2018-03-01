c7626bd498c3ceb689752e36bc20cb5c8c75652c
Francois Catuhe <fcatuhe@gmail.com>
2018-03-01 14:31:33 +0100

    move komponent to app/frontend

--- config/application.rb
+++ config/application.rb
@@ -41,6 +41,8 @@ module CookoonWeb
       Devise::Mailer.layout 'mailer'
     end
 
+    config.komponent.root = Rails.root.join('app/frontend')
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
--- frontend/components/index.js
+++
@@ -1,1 +0,0 @@
-import "components/availability/availability";
