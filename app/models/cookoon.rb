0a98da4ce7b58f9605dd8d95dd774ea9a4450b7d
Francois Catuhe <fcatuhe@gmail.com>
2018-03-02 18:27:53 +0100

    include filterable in Cookoon and add scope

--- app/models/cookoon.rb
+++ app/models/cookoon.rb
@@ -1,4 +1,5 @@
 class Cookoon < ApplicationRecord
+  include Filterable
   include TimeRangeBuilder
 
   scope :displayable_on_index, -> { joins(:user).where.not(users: { stripe_account_id: nil }) }
@@ -6,6 +7,7 @@ class Cookoon < ApplicationRecord
   scope :available_in, ->(range) { without_reservation_in(range).without_availabilty_in(range) }
   scope :without_reservation_in, ->(range) { where.not(id: Reservation.overlapping(range).pluck(:cookoon_id)) }
   scope :without_availabilty_in, ->(range) { where.not(id: Availability.overlapping(range).unavailable.pluck(:cookoon_id)) }
+  scope :near_default_radius, ->(address) { near(address, UserSearch.default.radius) }
   scope :created_in_day_range_around, ->(date_time) { where created_at: day_range(date_time) }
 
   CATEGORIES = %w[Appartement Maison Jardin Loft Terrasse Toit Villa].freeze
