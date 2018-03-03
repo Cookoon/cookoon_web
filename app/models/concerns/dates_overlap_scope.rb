b46fedb99599ff96e2822d7d7f94a292e3065975
Francois Catuhe <fcatuhe@gmail.com>
2018-03-03 15:43:52 +0100

    fix sql query

--- app/models/concerns/dates_overlap_scope.rb
+++ app/models/concerns/dates_overlap_scope.rb
@@ -7,7 +7,7 @@ module DatesOverlapScope
       where(<<~SQL, range_start: range.first, range_end: range.last)
         (start_at BETWEEN :range_start AND :range_end)
         OR (end_at BETWEEN :range_start AND :range_end)
-        OR (:range_start < start_at AND :range_end > end_at)
+        OR (start_at < :range_start AND :range_end < end_at)
       SQL
     end
   end
