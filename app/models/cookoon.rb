5cbf9eb3abc9042d376831f7aeba229e149fb43f
Francois Catuhe <fcatuhe@gmail.com>
2018-03-05 11:00:00 +0100

    include only accepted reservations in cookoon filter

--- app/models/cookoon.rb
+++ app/models/cookoon.rb
@@ -7,7 +7,7 @@ class Cookoon < ApplicationRecord
   scope :accomodates_for, ->(people_count) { where('capacity >= ?', people_count) }
   scope :near_default_radius, ->(address) { near(address, UserSearch.default.radius) }
   scope :available_in, ->(range) { without_reservation_in(range).without_availabilty_in(range) }
-  scope :without_reservation_in, ->(range) { where.not(id: Reservation.overlapping(range).pluck(:cookoon_id).uniq) }
+  scope :without_reservation_in, ->(range) { where.not(id: Reservation.accepted.overlapping(range).pluck(:cookoon_id).uniq) }
   scope :without_availabilty_in, ->(range) { where.not(id: Availability.unavailable.overlapping(range).pluck(:cookoon_id).uniq) }
   scope :created_in_day_range_around, ->(date_time) { where created_at: day_range(date_time) }
 
