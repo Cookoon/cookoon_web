26f2abdcaca4063b83e168a0dcf7870833c763a8
Charles Pernet <charlespernet@gmail.com>
2018-03-05 16:36:47 +0100

    Clean autocomplete_controller

--- app/frontend/controllers/google_maps_autocompletes_controller.js
+++ app/frontend/controllers/google_maps_autocompletes_controller.js
@@ -1,12 +1,7 @@
 import { Controller } from 'stimulus';
 
 export default class extends Controller {
-  static targets = [
-    'addressInput',
-    'formattedAddress',
-    'inputAddress',
-    'shortAddress'
-  ];
+  static targets = ['addressInput', 'formattedAddress', 'shortAddress'];
 
   connect() {
     this.autocomplete = new google.maps.places.Autocomplete(
@@ -40,67 +35,15 @@ export default class extends Controller {
     const place = this.autocomplete.getPlace();
 
     if (this.hasFormattedAddressTarget) {
-      return (this.formattedAddressTarget.value = place.formatted_address);
+      this.formattedAddressTarget.value = place.formatted_address;
     }
 
-    const addressComponents = this.buildAddressComponents(place);
-
-    if (this.hasInputAddressTarget) {
-      this.inputAddressTarget.value = addressComponents.inputAddress;
+    if (this.hasAddressInputTarget) {
+      this.addressInputTarget.value = place.formatted_address;
     }
 
     if (this.hasShortAddressTarget) {
-      this.shortAddressTarget.textContent = addressComponents.shortAddress;
+      this.shortAddressTarget.textContent = place.name;
     }
   };
-
-  buildAddressComponents(place) {
-    let addressComponents = {};
-
-    place.address_components.forEach(addressComponent => {
-      addressComponent.types.forEach(type => {
-        switch (type) {
-          case 'street_number':
-            return (addressComponents.streetNumber =
-              addressComponent.long_name);
-          case 'route':
-            return (addressComponents.route = addressComponent.long_name);
-          case 'postal_code':
-            return (addressComponents.postalCode = addressComponent.long_name);
-          case 'locality':
-            return (addressComponents.city = addressComponent.long_name);
-          case 'country_code':
-            return (addressComponents.countryCode =
-              addressComponent.short_name);
-          case 'country':
-            return (addressComponents.country = addressComponent.long_name);
-        }
-      });
-    });
-
-    addressComponents.inputAddress = this.buildInputAddress(addressComponents);
-    addressComponents.shortAddress = this.buildShortAddress(addressComponents);
-
-    return addressComponents;
-  }
-
-  buildInputAddress({ streetNumber, route, city }) {
-    if (streetNumber && route) {
-      return streetNumber + ' ' + route + ', ' + city;
-    } else if (route) {
-      return route + ', ' + city;
-    } else {
-      return city;
-    }
-  }
-
-  buildShortAddress({ streetNumber, route, city }) {
-    if (streetNumber && route) {
-      return streetNumber + ' ' + route;
-    } else if (route) {
-      return route;
-    } else {
-      return city;
-    }
-  }
 }
