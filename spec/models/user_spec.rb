5fd27c5f670def183bdb502c0e70623190befb44
Charles Pernet <charlespernet@gmail.com>
2018-03-06 12:12:46 +0100

    Add User Factory + first specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with a first name, last name, email and phone_number' do
+    user = build(:user)
+    expect(user).to be_valid
+  end
+
+  it 'is invalid without a first name' do
+    user = build(:user, first_name: nil)
+    user.valid?
+    expect(user.errors).to include(:first_name)
+  end
+
+  it 'is invalid without a last name' do
+    user = build(:user, last_name: nil)
+    user.valid?
+    expect(user.errors).to include(:last_name)
+  end
+
+  it 'is invalid without a phone number' do
+    user = build(:user, phone_number: nil)
+    user.valid?
+    expect(user.errors).to include(:phone_number)
+  end
+
+  it 'returns a full name' do
+    user = build(:user)
+    expect(user.full_name).to eq('Aaron Sumner')
+  end
 end
