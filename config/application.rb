6da470aeb236756e82eab1af12f85cde802eea6f
Charles Pernet <charlespernet@gmail.com>
2018-03-06 10:14:28 +0100

    Config application for tests

--- config/application.rb
+++ config/application.rb
@@ -49,5 +49,13 @@ module CookoonWeb
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.generators do |g|
+      g.test_framework :rspec,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: false
+    end
   end
 end
