283dc90e7c1a4ddcbef598cafc803bba934b6db7
Charles Pernet <charlespernet@gmail.com>
2018-03-09 14:52:59 +0100

    Add with_stripe_account trait to User Factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,5 +5,9 @@ FactoryBot.define do
     sequence(:email) { |n| "tester#{n}@example.com" }
     phone_number '0636686565'
     password 'plopplop'
+
+    trait :with_stripe_account do
+      stripe_account_id 'acct_1BdgAdJBhuyFUZoA'
+    end
   end
 end
