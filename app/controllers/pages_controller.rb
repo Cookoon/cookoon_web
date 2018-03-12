8b68a6469c714aa53824f3aa399f18c3bc864f7b
Francois Catuhe <fcatuhe@gmail.com>
2018-03-12 10:20:18 +0100

    refactor routes and app association files

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, except: :support
-  layout 'naked', except: :support
+  skip_before_action :authenticate_user!, only: %i[home setcookies]
+  layout 'naked', only: %i[home setcookies]
 
   def home; end
 
@@ -9,15 +9,5 @@ class PagesController < ApplicationController
   def setcookies
     lat_lng = "#{params[:lat]}|#{params[:lng]}"
     cookies[:lat_lng] ||= { value: lat_lng, expires: 5.hours.from_now }
-  end
-
-  def apple_app_site_association
-    association_json = File.read(Rails.public_path + 'apple-app-site-association')
-    render json: association_json, content_type: 'application/json'
-  end
-
-  def android_assetlinks
-    association_json = File.read(Rails.public_path + 'android_assetlinks.json')
-    render json: association_json, content_type: 'application/json'
   end
 end
