32b5a2e4b4eabd5248a5f69a1a152e361f72746d
Francois Catuhe <fcatuhe@gmail.com>
2018-03-15 18:42:44 +0100

    add flash modal for invitation accepted

--- app/responders/invitations_responder.rb
+++ app/responders/invitations_responder.rb
@@ -1,6 +1,7 @@
 module InvitationsResponder
   def to_html
     controller.flash[:invitation_sent] = true if post? && !has_errors?
+    controller.flash[:invitation_accepted] = true if patch? && !has_errors?
     super
   end
 end
