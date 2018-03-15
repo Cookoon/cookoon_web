b8bbb006690440a69c1bbf181b98dff0e79a2f3e
Francois Catuhe <fcatuhe@gmail.com>
2018-03-15 18:23:04 +0100

    udpate navbar with component approach

--- app/frontend/components/index.js
+++ app/frontend/components/index.js
@@ -1,1 +1,2 @@
 import 'components/availability/availability';
+import "components/navbar_cookoon/navbar_cookoon";
---
+++ app/frontend/components/navbar_cookoon/navbar_cookoon_component.rb
@@ -0,0 +1,22 @@
+module NavbarCookoonComponent
+  extend ComponentHelper
+  property :type, default: :menu # :back, :fixed_back
+  property :back_url
+
+  def display_type
+    if @back_url.blank?
+      @type
+    else
+      @type == :menu ? :back : @type
+    end.to_s
+  end
+
+  def back_link
+    return @back_url if @back_url.present?
+    'javascript:history.back()'
+  end
+
+  def notifications?
+    notifiable_reservations? || notifiable_reservation_requests?
+  end
+end
