7782d7bfe11e49b55c5cbc1d87badb4d9e70a6da
Charles Pernet <charlespernet@gmail.com>
2018-03-20 11:46:13 +0100

    Clean Job

--- app/jobs/reservations_cleanup_job.rb
+++ app/jobs/reservations_cleanup_job.rb
@@ -1,11 +1,29 @@
 class ReservationsCleanupJob < ApplicationJob
   queue_as :default
 
-  def perform(_scheduled_time)
-    without_mail = Reservation.pending.to_cancel
-    with_mail = Reservation.paid.to_cancel
-    without_mail.or(with_mail).each(&:cancelled!)
-    # Send mail only when reservation was paid
-    # with_mail.each { PENDING SEND_MAIL }
+  def perform
+    cleanup_pending_unpayable
+    cleanup_short_notice
+    cleanup_stripe_will_not_capture
+  end
+
+  private
+
+  def cleanup_pending_unpayable
+    Reservation.pending_unpayable.each(&:dead!)
+  end
+
+  def cleanup_short_notice
+    Reservation.short_notice.each do |reservation|
+      reservation.cancelled!
+      # TODO : MAILER CONSEIL RESERVER PLUS TOT
+    end
+  end
+
+  def cleanup_stripe_will_not_capture
+    Reservation.stripe_will_not_capture.each do |reservation|
+      reservation.cancelled!
+      # TODO : MAILER HOTE N'A PAS REAGI
+    end
   end
 end
