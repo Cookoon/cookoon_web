235e2145c9cf4fee2ce6045a50a4293e67177a24
Charles Pernet <charlespernet@gmail.com>
2018-03-20 12:17:52 +0100

    WIP Reservation spec

---
+++ spec/models/reservation_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe Reservation, type: :model do
+  it 'has a valid factory' do
+    reservation = build(:reservation)
+    expect(reservation).to be_valid
+  end
+
+  it 'is invalid without a start date' do
+    reservation = build(:reservation, start_at: nil)
+    reservation.valid?
+    expect(reservation.errors[:start_at]).to include('doit être rempli(e)')
+  end
+
+  it 'is invalid without a duration' do
+    reservation = build(:reservation, duration: nil)
+    reservation.valid?
+    expect(reservation.errors[:duration]).to include('doit être rempli(e)')
+  end
+end
