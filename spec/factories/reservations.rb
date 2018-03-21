a27a680c06b46d86b5a1e7f5ccbf24bb6f550cdd
Charles Pernet <charlespernet@gmail.com>
2018-03-21 10:00:28 +0100

    Add traits to reservations factory

--- spec/factories/reservations.rb
+++ spec/factories/reservations.rb
@@ -2,7 +2,19 @@ FactoryBot.define do
   factory :reservation do
     association :cookoon
     association :user
-    start_at Time.zone.now.in(10.days)
+    start_at 10.days.from_now
     duration 2
+
+    trait :paid do
+      status :paid
+    end
+
+    trait :created_two_days_ago do
+      created_at 2.days.ago
+    end
+
+    trait :created_ten_days_ago do
+      created_at 10.days.ago
+    end
   end
 end
