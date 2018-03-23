c05ce5a77c66d432002f9c75859d3cf1a0428b2f
Charles Pernet <charlespernet@gmail.com>
2018-03-23 17:49:31 +0100

    Update Service to manage discount

--- app/models/service.rb
+++ app/models/service.rb
@@ -2,8 +2,18 @@ class Service < ApplicationRecord
   belongs_to :reservation
 
   monetize :price_cents
+  monetize :discount_amount_cents
+  monetize :charge_amount_cents
 
   enum status: %i[quote paid]
 
   validates :content, presence: true
+
+  def discount_used?
+    discount_amount_cents.positive?
+  end
+
+  def charge_amount_cents
+    price_cents - discount_amount_cents
+  end
 end
