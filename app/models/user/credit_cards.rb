f4c0cdabe22b9df9b708a9d07637514b27f76bab
Charles Pernet <charlespernet@gmail.com>
2018-03-30 17:42:13 +0200

    Add methods to manage credit cards in User::CreditCards

--- app/models/user/credit_cards.rb
+++ app/models/user/credit_cards.rb
@@ -1,19 +1,27 @@
-class User::CreditCards
-  attr_reader :user, :customer
-  delegate :stripe_customer, to: :user
+class User
+  class CreditCards
+    attr_reader :user, :errors
+    delegate :stripe_customer, to: :user
 
-  def initialize(user)
-    @user = user
-  end
+    def initialize(user)
+      @user = user
+    end
 
-  def list
-    retrieve_sources
-  end
+    def list
+      user.retrieve_stripe_sources
+    end
 
-  private
+    def add(token)
+      user.create_stripe_customer unless stripe_customer
+      user.create_stripe_source(token)
+    end
 
-  def retrieve_sources
-    return [] unless stripe_customer
-    stripe_customer.sources.all(object: 'card')
+    def destroy(card)
+      user.destroy_stripe_source(card)
+    end
+
+    def default(card)
+      user.default_stripe_source(card)
+    end
   end
 end
