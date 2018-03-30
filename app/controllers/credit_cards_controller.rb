e0bd2a5f3883d98c1a6211bdaefa0ee1cf9cd5b0
Charles Pernet <charlespernet@gmail.com>
2018-03-30 17:39:57 +0200

    Remove paymentService from controller + conditionnal redirection after adding card

--- app/controllers/credit_cards_controller.rb
+++ app/controllers/credit_cards_controller.rb
@@ -2,39 +2,43 @@ class CreditCardsController < ApplicationController
   # Try a better approach than skiping once done
   skip_after_action :verify_policy_scoped
   skip_after_action :verify_authorized
-  before_action :set_payment_service
 
   def index
     @stripe_sources = current_user.credit_cards
   end
 
   def create
-    card = @payment_service.add_source_to_customer
-    @payment_service.default_card(card) if params[:credit_card][:default] == '1'
+    card = credit_card.add(credit_card_params[:stripe_token])
+    credit_card.default(card) if set_card_as_default?
     handle_redirection(card)
   end
 
   def destroy
-    @payment_service.destroy_card(params[:id])
+    credit_card.destroy(params[:id])
     redirect_to credit_cards_path
   end
 
   private
 
   def handle_redirection(card)
-    if card
-      redirect_to credit_cards_path
-    else
-      @stripe_sources = @payment_service.user_sources
-      flash.now[:alert] = @payment_service.displayable_errors
-      render :index
+    referer_infos = Rails.application.routes.recognize_path(request.referer)
+    if referer_infos.key?(:reservation_id) && card
+      return redirect_to new_reservation_payment_path(referer_infos[:reservation_id])
     end
+
+    flash[:alert] = current_user.errors.full_messages.join(', ') unless card
+    redirect_to credit_cards_path
   end
 
-  def set_payment_service
-    @payment_service = StripePaymentService.new(
-      user: current_user,
-      token: params.dig(:credit_card, :stripe_token)
-    )
+  def credit_card_params
+    params.require(:credit_card)
+  end
+
+  def credit_card
+    current_user.credit_card
+  end
+
+  def set_card_as_default?
+    credit_card_params[:default] == '1'
   end
 end
