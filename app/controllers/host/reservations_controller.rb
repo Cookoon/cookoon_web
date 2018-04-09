cb0971c25f45e2cfbfb303b11605642b57bb99c2
Charles Pernet <charlespernet@gmail.com>
2018-04-09 17:29:16 +0200

    Host::ReservationsController Lint

--- app/controllers/host/reservations_controller.rb
+++ app/controllers/host/reservations_controller.rb
@@ -1,5 +1,5 @@
 class Host::ReservationsController < ApplicationController
-  before_action :find_reservation, only: [:edit, :update]
+  before_action :find_reservation, only: %i[edit update]
 
   def index
     reservations = policy_scope([:host, Reservation]).includes(:cookoon, user: :photo_files)
@@ -10,7 +10,7 @@ class Host::ReservationsController < ApplicationController
   def edit; end
 
   def update
-    status = params["accept"] ? :accepted : :refused
+    status = params['accept'] ? :accepted : :refused
     merged_params = reservation_params.merge(status: status)
 
     if @reservation.update(merged_params)
@@ -18,16 +18,16 @@ class Host::ReservationsController < ApplicationController
         payment = Reservation::Payment.new(@reservation)
         if payment.capture
           @reservation.notify_users_after_confirmation
-          flash[:notice] = "Vous avez accepté la réservation"
+          flash[:notice] = 'Vous avez accepté la réservation'
         else
-          # TODO : Essayer à nouveau de capturer la charge ou afficher une erreur.
-          # Attention au status de la reservation qui peut etre erronné si la charge n'est pas correctement capturée
+          # TODO : Essayer a nouveau de capturer la charge ou afficher une erreur.
+          # Attention au status de la reservation qui peut etre erronne si la charge n'est pas correctement capturee
           flash[:alert] = payment_service.displayable_errors
         end
       else
         @reservation.refund_discount_to_user if @reservation.payment.discount_used?
         ReservationMailer.refused_to_tenant(@reservation).deliver_later
-        flash[:notice] = "Vous avez refusé la réservation"
+        flash[:notice] = 'Vous avez refusé la réservation'
       end
 
       redirect_to host_reservations_path
