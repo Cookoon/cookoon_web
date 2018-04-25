2e3932d36d70fba4d3d9d2d6c49d397392c8c6d5
Francois Catuhe <fcatuhe@gmail.com>
2018-04-25 12:28:28 +0200

    wip search integration

--- app/frontend/components/search/search_controller.js
+++ app/frontend/components/search/search_controller.js
@@ -1,13 +1,30 @@
-import { Controller } from "stimulus";
+import { Controller } from 'stimulus';
+import Slider from 'bootstrap-slider';
+import 'bootstrap-slider/dist/css/bootstrap-slider';
+import './slider';
 
 export default class extends Controller {
-  static targets = [
-    'body',
-    'example'
-  ]
+  static targets = ['body', 'durationInput', 'example', 'peopleInput'];
+
+  connect() {
+    this.durationSlider = new Slider(this.durationInputTarget, {
+      value: 2,
+      ticks: [2, 3, 5, 10],
+      ticks_labels: ['2h', '3h', '5h', '10h'],
+      ticks_positions: [0, 33.33, 66.66, 100],
+      ticks_snap_bounds: 5
+    });
+    this.peopleSlider = new Slider(this.peopleInputTarget, {
+      value: 2,
+      ticks: [2, 4, 6, 8, 10, 12],
+      ticks_labels: ['2', '4', '6', '8', '10', '12']
+    });
+  }
 
   toggleBodyVisibility() {
     $(this.bodyTarget).slideToggle();
+    this.durationSlider.relayout();
+    this.peopleSlider.relayout();
     $(this.exampleTarget).slideToggle();
   }
 }
