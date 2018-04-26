16a64f754d62829e2a9655a7e824476b3d3e1294
Francois Catuhe <fcatuhe@gmail.com>
2018-04-26 23:58:17 +0200

    use search_params

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,8 +1,6 @@
 class SearchesController < ApplicationController
   def create
-    temp_params = {start_at: 3.days.from_now, people_count: 6, duration: 5}
-    # TODO : Replace temp_params by search_params when done
-    @search = current_user.searches.build(temp_params)
+    @search = current_user.searches.build(search_params)
     authorize @search
 
     @search.save
@@ -22,7 +20,6 @@ class SearchesController < ApplicationController
   def search_params
     params.require(:search)
           .permit(:start_at, :duration, :people_count)
-          .delocalize(start_at: :time)
   end
 
   def build_markers
