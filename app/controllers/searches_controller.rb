5ec1e9880b4fa961ee8ad53210615c7264fd7a2c
Charles Pernet <charlespernet@gmail.com>
2018-04-26 15:20:30 +0200

    Connect Search button to Search controller and redirect with actual search built

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,6 +1,8 @@
 class SearchesController < ApplicationController
   def create
-    @search = current_user.searches.build(search_params)
+    temp_params = {start_at: 3.days.from_now, people_count: 6, duration: 5}
+    # TODO : Replace temp_params by search_params when done
+    @search = current_user.searches.build(temp_params)
     authorize @search
 
     @search.save
