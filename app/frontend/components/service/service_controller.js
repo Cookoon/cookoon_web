01ee4c7866443b11aaba49a3bd1d85608a39cae7
Charles Pernet <charlespernet@gmail.com>
2018-04-30 15:09:57 +0200

    WIP Stimulus service controller

--- app/frontend/components/service/service_controller.js
+++ app/frontend/components/service/service_controller.js
@@ -1,7 +1,24 @@
 import { Controller } from "stimulus";
+import Rails from 'rails-ujs';
 
 export default class extends Controller {
+  static dataAttributes = ['category', 'selected'];
+
   connect() {
-    console.log("Hello, Stimulus!", this.element);
+
+  }
+
+  toggle() {
+    Rails.ajax({
+      url: this.data.get('url'),
+      type: this.data.get('method'),
+      data: { category: this.data.get('category') },
+      success: { console.log('success') },
+      error: { console.log('error') }
+    });
+
+    const priceChangedEvent = new Event('serviceChanged');
+    this.element.classList.toggle("service-icon-selected");
+    document.dispatchEvent(priceChangedEvent);
   }
 }
