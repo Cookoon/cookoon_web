2d5c2ca9e9d9c2df8a68e78502d9ef00445f165b
Charles Pernet <charlespernet@gmail.com>
2018-05-02 17:28:45 +0200

    Update price dynamicaly on payment page

--- app/frontend/components/service/service_controller.js
+++ app/frontend/components/service/service_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "stimulus";
 import Rails from 'rails-ujs';
 
 export default class extends Controller {
-  static priceChangedEvent = new Event('serviceChanged');
+  static priceChangedEvent = new Event('serviceChanged', {'bubbles': true});
 
   toggle() {
     const data = new FormData();
@@ -17,12 +17,11 @@ export default class extends Controller {
         this.data.set('url', url);
         this.data.set('method', method);
         this.data.set('selected', selected);
+        this.element.dispatchEvent(this.constructor.priceChangedEvent);
       },
       error: (_jqXHR, _textStatus, errorThrown) => {
         console.log(errorThrown);
       }
     });
-
-    document.dispatchEvent(this.constructor.priceChangedEvent);
   }
 }
