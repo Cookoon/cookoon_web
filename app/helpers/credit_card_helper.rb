732fc90a487d8fb41dfd76d2a2e136d6c17a1eaf
Francois Catuhe <fcatuhe@gmail.com>
2018-05-07 12:26:55 +0200

    improve credit_cards_helper

--- app/helpers/credit_card_helper.rb
+++
@@ -1,7 +0,0 @@
-module CreditCardHelper
-  def display_credit_card_for(card_data)
-    brand = card_data.brand.downcase
-    brand = 'amex' if card_data.brand == 'American Express'
-    "<i class='fab fa-cc-#{brand}' aria-hidden='true'></i> •••• #{card_data.last4}".html_safe
-  end
-end
