c60b95a4f90445bda951025f52e6d11f5ab936bc
Charles Pernet <charlespernet@gmail.com>
2018-05-09 17:45:33 +0200

    Add bund of useful price methods in Ephemeral

--- app/models/ephemeral.rb
+++ app/models/ephemeral.rb
@@ -5,7 +5,30 @@ class Ephemeral < ApplicationRecord
   validates :start_at, presence: true
   validates :duration, presence: true
   validates :people_count, presence: true
-  validates :service_price, presence: true
+  # TODO : CP 9may2018 should we monetize this instead ?
+  validates :service_price_cents, presence: true
+
+  monetize :service_price_cents
+  monetize :price_cents
+  monetize :rental_price_cents
+  monetize :payment_amount_cents
 
   enum status: %i[inactive available unavailable]
+
+  def payment(options = {})
+    Ephemeral::Payment.new(self, options)
+  end
+
+  # TODO CP 9may2018 Refacto?
+  def price_cents
+    rental_price_cents + service_price_cents
+  end
+
+  def rental_price_cents
+    cookoon.price_cents * duration
+  end
+
+  def payment_amount_cents
+    price_cents + ((rental_price_cents * Reservation::DEFAULTS[:tenant_fee_rate]).round)
+  end
 end
