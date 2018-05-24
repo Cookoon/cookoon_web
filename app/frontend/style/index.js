2749a23dad05593d4d5db1acef95015d1cc79fca
Francois Catuhe <fcatuhe@gmail.com>
2018-05-24 17:30:41 +0200

    move scss from sprocket to webpack

---
+++ app/frontend/style/index.js
@@ -0,0 +1,6 @@
+import './application';
+
+import '@fortawesome/fontawesome-free/scss/fontawesome';
+import '@fortawesome/fontawesome-free/scss/fa-brands';
+import '@fortawesome/fontawesome-free/scss/fa-regular';
+import '@fortawesome/fontawesome-free/scss/fa-solid';
