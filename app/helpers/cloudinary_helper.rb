29981567b37f86f0ccb902779a4762bbdaf38391
Francois Catuhe <fcatuhe@gmail.com>
2018-05-28 19:40:22 +0200

    remove already assigned Cloudinary constant from helper

--- app/helpers/cloudinary_helper.rb
+++ app/helpers/cloudinary_helper.rb
@@ -1,6 +1,4 @@
 module CloudinaryHelper
-  CLOUDINARY_JS_CONFIG_PARAMS = %i[api_key cloud_name private_cdn secure_distribution cdn_subdomain].freeze
-
   def cloudinary_config_params
     params = {}
     CLOUDINARY_JS_CONFIG_PARAMS.each do |param|
