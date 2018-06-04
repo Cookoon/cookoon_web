1f0ae379e47d5237e1ccdcfa797bd53656a2ac6a
Charles Pernet <charlespernet@gmail.com>
2018-06-04 13:23:36 +0200

    Wip Reservation message for host

--- app/controllers/reservations/messages_controller.rb
+++ app/controllers/reservations/messages_controller.rb
@@ -1,7 +1,18 @@
 class Reservations::MessagesController < ApplicationController
+  before_action :find_reservation
+  #TODO CP 4JUN Implement pundit when done
+  skip_after_action :verify_policy_scoped
+  skip_after_action :verify_authorized
+
   def new; end
 
   def create
     # redirect_to reservations_path, #flash: { payment_succeed: true }
+  end
+
+  private
+
+  def find_reservation
+    @reservation = Reservation.find params[:reservation_id]
   end
 end
