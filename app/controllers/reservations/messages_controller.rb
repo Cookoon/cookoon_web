ce07f4cdf2e257ae1711bd5b75b0938522e9c78d
Charles Pernet <charlespernet@gmail.com>
2018-06-04 15:15:29 +0200

    Pundit and actual redirections

--- app/controllers/reservations/messages_controller.rb
+++ app/controllers/reservations/messages_controller.rb
@@ -1,18 +1,21 @@
 class Reservations::MessagesController < ApplicationController
   before_action :find_reservation
-  #TODO CP 4JUN Implement pundit when done
-  skip_after_action :verify_policy_scoped
-  skip_after_action :verify_authorized
 
   def new; end
 
   def create
-    # redirect_to reservations_path, #flash: { payment_succeed: true }
+    @reservation.update(reservation_params)
+    redirect_to home_path
   end
 
   private
 
   def find_reservation
     @reservation = Reservation.find params[:reservation_id]
+    authorize @reservation
+  end
+
+  def reservation_params
+    params.require(:reservation).permit(:message_for_host)
   end
 end
