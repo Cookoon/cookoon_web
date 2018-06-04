839996f09c19e15022e05c1ad5f91915ebe5fc00
Charles Pernet <charlespernet@gmail.com>
2018-06-04 11:31:57 +0200

    Migration to add host message to reservations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180502123233) do
+ActiveRecord::Schema.define(version: 2018_06_04_092928) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -141,6 +141,7 @@ ActiveRecord::Schema.define(version: 20180502123233) do
     t.datetime "end_at"
     t.text "guests_message"
     t.integer "people_count"
+    t.text "message_for_host"
     t.index ["cookoon_id"], name: "index_reservations_on_cookoon_id"
     t.index ["user_id"], name: "index_reservations_on_user_id"
   end
