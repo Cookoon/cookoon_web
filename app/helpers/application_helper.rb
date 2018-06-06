fb7dad3fc8585903963bc9025e6ad2bc79b9b227
Charles Pernet <charlespernet@gmail.com>
2018-06-06 17:12:13 +0200

    Add basic tel_to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,9 @@ module ApplicationHelper
   def turbolinks_cache_control_meta_tag
     tag :meta, name: 'turbolinks-cache-control', content: @turbolinks_cache_control || 'cache'
   end
+
+  def tel_to(text)
+    groups = text.to_s.scan(/(?:^\+)?\d+/)
+    link_to text, "tel:#{groups.join '-'}"
+  end
 end
