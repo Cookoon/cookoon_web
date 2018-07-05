417b9cb106d7aee6cc4a83d537533d3e9e6bf454
Charles Pernet <charlespernet@gmail.com>
2018-07-05 16:37:12 +0200

    Slack notifier service

--- app/services/slack_notifier_service.rb
+++ app/services/slack_notifier_service.rb
@@ -1,4 +1,7 @@
 class SlackNotifierService
+  include DatetimeHelper
+  include ActionView::Helpers::TranslationHelper
+
   WEBHOOK_URL = 'https://hooks.slack.com/services/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'
 
   def initialize(attributes)
@@ -9,11 +12,14 @@ class SlackNotifierService
   end
 
   def notify
+    return unless message
     notifier = Slack::Notifier.new WEBHOOK_URL, notifier_options
     notifier.ping message
   end
 
-  private
+  # private
+
+  attr_reader :reservation, :cookoon, :host, :tenant
 
   def notifier_options
     {
@@ -23,6 +29,23 @@ class SlackNotifierService
   end
 
   def message
-    "Hello random"
+    case reservation.status.to_sym
+    when :paid
+      "[NOUVELLE DEMANDE] #{cookoon.name} le #{formatted_date} par #{tenant.full_name}"
+    when :accepted
+      "[ACCEPTATION] la location pour #{cookoon.name} le #{formatted_date} vient d'être acceptée par #{host.full_name}"
+    when :refused
+      "[REFUS] la location pour #{cookoon.name} le #{formatted_date} vient d'être refusée par #{host.full_name}"
+    when :ongoing
+      "[EN COURS] la location pour #{cookoon.name} par #{tenant.full_name} vient de démarrer"
+    when :passed
+      "[FIN] la location pour #{cookoon.name} par #{tenant.full_name} vient de se terminer"
+    when :cancelled
+      "[ANNULATION] la location pour #{cookoon.name} le #{formatted_date} vient d'être annulée"
+    end
+  end
+
+  def formatted_date
+    display_datetime_for(reservation.start_at, join_expression: 'à', without_year: true)
   end
 end
