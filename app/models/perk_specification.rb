f2a910a0f01a2df3884d437a37063e635919fa99
Charles Pernet <charlespernet@gmail.com>
2018-07-11 10:18:35 +0200

    Add uniqueness validation on perk_specification

--- app/models/perk_specification.rb
+++ app/models/perk_specification.rb
@@ -1,6 +1,7 @@
 class PerkSpecification < ApplicationRecord
   validates :name, presence: true
   validates :icon_name, presence: true
+  validates :name, uniqueness: true
 
   has_many :perks, dependent: :destroy
 end
