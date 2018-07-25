12e3899fff02bbe1b3597b9be4e3dba8077d6e9e
Charles Pernet <charlespernet@gmail.com>
2018-07-25 11:43:24 +0200

    Routing for Pro Namespace

--- config/routes.rb
+++ config/routes.rb
@@ -72,6 +72,11 @@ Rails.application.routes.draw do
   # -------- PRO NAMESPACE ---------
   namespace :pro do
     get :home, controller: :pages
+    resources :quotes, only: %i[index create update] do
+      resources :proposals, only: %i[show edit update]
+    end
+    resources :services, only: %i[index]
+    resources :cookoons, only: %i[index]
   end
 
 
