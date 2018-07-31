511a88a623c8e9a8ecef0776c070642c403ae93d
Charles Pernet <charlespernet@gmail.com>
2018-07-31 15:48:53 +0200

    fix navbar css

--- app/controllers/pro/cookoons_controller.rb
+++ app/controllers/pro/cookoons_controller.rb
@@ -1,5 +1,8 @@
 module Pro
   class CookoonsController < ApplicationController
+    skip_after_action :verify_policy_scoped
+    skip_after_action :verify_authorized
+    
     def index; end
   end
 end
