c5e740ba2b5dd7b089cc746435799a3664ed65d8
Charles Pernet <charlespernet@gmail.com>
2018-07-31 16:11:50 +0200

    Sample cookoons collection on pro_cookoons index

--- app/controllers/pro/cookoons_controller.rb
+++ app/controllers/pro/cookoons_controller.rb
@@ -2,7 +2,9 @@ module Pro
   class CookoonsController < ApplicationController
     skip_after_action :verify_policy_scoped
     skip_after_action :verify_authorized
-    
-    def index; end
+
+    def index
+      @cookoons = Cookoon.includes(:photo_files).decorate.first(5)
+    end
   end
 end
