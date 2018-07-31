a8cba4c243432826727f15507fc9fb28bd4bd66a
Charles Pernet <charlespernet@gmail.com>
2018-07-31 16:50:11 +0200

    Add #show to pro/cookoons_controller

--- app/controllers/pro/cookoons_controller.rb
+++ app/controllers/pro/cookoons_controller.rb
@@ -6,5 +6,10 @@ module Pro
     def index
       @cookoons = Cookoon.includes(:photo_files).decorate.first(5)
     end
+
+    def show
+      @cookoon = Cookoon.find(params[:id])
+      @marker = { lat: @cookoon.latitude, lng: @cookoon.longitude }
+    end
   end
 end
