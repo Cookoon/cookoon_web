633773558e2bb404360036f0f5cbe9bc9dbfc4db
Charles Pernet <charlespernet@gmail.com>
2018-08-01 10:50:36 +0200

    Replace jquery by JS in cookoons/new.js + shallow routing

--- config/routes.rb
+++ config/routes.rb
@@ -76,10 +76,8 @@ Rails.application.routes.draw do
     resources :quotes, only: %i[index create update] do
       resources :proposals, only: %i[show edit update]
       resources :services, only: %i[index]
-      resources :cookoons, only: %i[index]
+      resources :cookoons, only: %i[index show], shallow: true
     end
-
-    resources :cookoons, only: %i[show]
   end
 
 
