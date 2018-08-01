1438eadbe8dfc84642815153ca58eda3805cb798
Charles Pernet <charlespernet@gmail.com>
2018-08-01 11:34:22 +0200

    Remove useless policy in Pro::PagesController#home

--- app/controllers/pro/pages_controller.rb
+++ app/controllers/pro/pages_controller.rb
@@ -1,7 +1,6 @@
 module Pro
   class PagesController < ApplicationController
     def home
-      authorize([:pro, current_user], :pro?)
       @quote = Quote.new
       @cookoons = Cookoon.random.limit(3).decorate
     end
