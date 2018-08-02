01c2ceb5ed860bfd3e4006b0b72bd648ec7e5db0
Francois Catuhe <fcatuhe@gmail.com>
2018-08-02 10:45:12 +0200

    use quote attributes to select cookoons

--- app/controllers/pro/cookoons_controller.rb
+++ app/controllers/pro/cookoons_controller.rb
@@ -5,9 +5,17 @@ module Pro
     skip_after_action :verify_authorized
 
     def index
-      # TODO CP 31 JUL : Use quote to select cookoons
-      @quote = Pro::Quote.find(params[:quote_id]).decorate
-      @cookoons = Cookoon.limit(5).includes(:photo_files).decorate
+      quote = Pro::Quote.find(params[:quote_id])
+      @quote = quote.decorate
+
+      filtering_params = {
+        accomodates_for: quote.people_count,
+        available_in: (quote.start_at..@quote.end_at)
+      }
+      @cookoons = policy_scope(Cookoon)
+                  .includes(:photo_files)
+                  .filter(filtering_params)
+                  .decorate
     end
 
     def show
