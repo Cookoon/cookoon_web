f2bd9e0eb14dd89d9466d042eaf3f88388d70681
Charles Pernet <charlespernet@gmail.com>
2018-08-02 16:25:33 +0200

    Add controller and view to create QuoteCookoon

---
+++ app/controllers/pro/quote_cookoons_controller.rb
@@ -0,0 +1,22 @@
+module Pro
+  class QuoteCookoonsController < ApplicationController
+    skip_after_action :verify_policy_scoped
+    skip_after_action :verify_authorized
+
+    def create
+      @quote = Quote.includes(:cookoons).find(params[:quote_id])
+      @cookoon = Cookoon.find(params[:cookoon_id])
+      @quote.quote_cookoons.create(cookoon: @cookoon)
+      @quote.reload
+      return redirect_to pro_quote_services_path(@quote) if @quote.cookoons.count == 2
+    end
+
+    def update; end
+
+    private
+
+    def quote_params
+      params.require(:pro_quote).permit(:start_at, :duration, :people_count)
+    end
+  end
+end
