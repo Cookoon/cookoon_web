c3206ca8b6d82422c5cd1d7db29cb691af2fe6bc
Charles Pernet <charlespernet@gmail.com>
2018-08-06 11:57:11 +0200

    Service Done

--- app/models/pro/service.rb
+++ app/models/pro/service.rb
@@ -1,3 +1,11 @@
-class Pro::Service < ApplicationRecord
-  belongs_to :pro_reservation
+module Pro
+  class Service < ApplicationRecord
+    belongs_to :reservation, class_name: 'Pro::Reservation', foreign_key: :pro_reservation_id, inverse_of: :services
+
+    validates :name, presence: true
+    validates :quantity, numericality: { only_integer: true, greater_than: 0 }
+
+    monetize :unit_price_cents
+    monetize :price_cents
+  end
 end
