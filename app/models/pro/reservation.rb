35647b13a804ffeae6d05c7030dbec4893ae7ca6
Charles Pernet <charlespernet@gmail.com>
2018-08-06 11:45:33 +0200

    Associations for Reservation

--- app/models/pro/reservation.rb
+++ app/models/pro/reservation.rb
@@ -1,4 +1,10 @@
-class Pro::Reservation < ApplicationRecord
-  belongs_to :pro_quote
-  belongs_to :cookoon
+module Pro
+  class Reservation < ApplicationRecord
+    belongs_to :quote, class_name: 'Pro::Quote', foreign_key: :pro_quote_id, inverse_of: :reservations
+    belongs_to :cookoon
+
+    has_many :services,
+             class_name: 'Pro::Service', inverse_of: :reservation,
+             foreign_key: :pro_reservation_id, dependent: :destroy
+  end
 end
