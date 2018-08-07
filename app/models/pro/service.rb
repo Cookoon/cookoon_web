48cc0cf155d27501d3e3f6b531391f7914e58abc
Francois Catuhe <fcatuhe@gmail.com>
2018-08-07 15:50:50 +0200

    basic implementation of price assignment

--- app/models/pro/service.rb
+++ app/models/pro/service.rb
@@ -7,5 +7,18 @@ module Pro
 
     monetize :unit_price_cents
     monetize :price_cents
+
+    before_save :assign_price
+    after_commit :save_reservation
+
+    private
+
+    def assign_price
+      self.price = quantity * unit_price
+    end
+
+    def save_reservation
+      reservation.save
+    end
   end
 end
