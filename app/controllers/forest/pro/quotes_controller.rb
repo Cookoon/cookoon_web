3901545cb32ce73e79712872e62eacf3d5d70a03
Francois Catuhe <fcatuhe@gmail.com>
2018-08-07 10:16:01 +0200

    implement create_draft_reservation Forest action

--- app/controllers/forest/pro/quotes_controller.rb
+++ app/controllers/forest/pro/quotes_controller.rb
@@ -2,7 +2,20 @@ module Forest
   module Pro
     class QuotesController < ForestLiana::ApplicationController
       def create_draft_reservation
-        head :no_content
+        quote = ::Pro::Quote.find(params.dig(:data, :attributes, :ids)&.first)
+
+        reservation = ::Pro::Reservation.create(
+          {
+            quote: quote,
+            cookoon: quote.cookoons.first
+          }.merge(quote.attributes.slice('start_at', 'duration', 'people_count'))
+        )
+
+        quote.services.each do |service|
+          reservation.services.create(name: service.category.capitalize, quantity: service.quantity)
+        end
+
+        render json: { success: 'Draft Pro::Reservation was created' }
       end
     end
   end
