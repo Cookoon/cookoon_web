ee9c43c4542bfaf436f8703372e61747798e7387
Francois Catuhe <fcatuhe@gmail.com>
2018-08-07 16:28:39 +0200

    decorate reservations and services prices

--- app/decorators/pro/reservation_decorator.rb
+++ app/decorators/pro/reservation_decorator.rb
@@ -1,6 +1,7 @@
 module Pro
   class ReservationDecorator < Draper::Decorator
     delegate_all
+    decorates_association :services
 
     def created_on(options = {})
       h.display_date_for(object.created_at, options)
@@ -24,6 +25,18 @@ module Pro
 
     def cookoon_price
       h.humanized_money_with_symbol object.cookoon_price
+    end
+
+    def services_price
+      h.humanized_money_with_symbol object.services_price
+    end
+
+    def fee
+      h.humanized_money_with_symbol object.fee
+    end
+
+    def price
+      h.humanized_money_with_symbol object.price
     end
   end
 end
