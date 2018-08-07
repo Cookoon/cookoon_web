9216965572ec07f0c9efd217ed91783a40550a20
Francois Catuhe <fcatuhe@gmail.com>
2018-08-07 10:11:55 +0200

    add quantity validation on Pro::QuoteService

--- app/models/pro/quote_service.rb
+++ app/models/pro/quote_service.rb
@@ -3,5 +3,7 @@ module Pro
     belongs_to :quote, class_name: 'Pro::Quote', foreign_key: 'pro_quote_id', inverse_of: :services
 
     enum category: %i[special catering chef corporate]
+
+    validates :quantity, numericality: { only_integer: true, greater_than: 0 }
   end
 end
