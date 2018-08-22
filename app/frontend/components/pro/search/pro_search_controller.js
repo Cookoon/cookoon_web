f0a0895c0a5821c2e6fed358cdfd97eadab74435
Charles Pernet <charlespernet@gmail.com>
2018-08-22 18:27:21 +0200

    Handle click event on search

--- app/frontend/components/pro/search/pro_search_controller.js
+++ app/frontend/components/pro/search/pro_search_controller.js
@@ -1,7 +1,35 @@
 import { Controller } from "stimulus";
 
 export default class extends Controller {
+  static targets = [
+    'durationSelection',
+    'selection',
+    'durationInput',
+    'countInput'
+  ];
+
   connect() {
     console.log("Hello, Stimulus!", this.element);
+  }
+
+  toggleSelection() {
+    event.target.querySelector('.pro-search-input-selection') &&
+      event.target.querySelector('.pro-search-input-selection').classList.toggle('d-none');
+  }
+
+  hideSelections() {
+    this.selectionTargets.forEach(element => {
+      if (element !== event.target) {
+        element.querySelector('.pro-search-input-selection').classList.add('d-none');
+      }
+    })
+  }
+
+  selectCount() {
+    this.countInputTarget.value = event.target.dataset.count;
+  }
+
+  selectDuration() {
+    this.durationInputTarget.value = event.target.dataset.duration;
   }
 }
