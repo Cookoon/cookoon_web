f3b07d2f3cd5881dc9d74061a3f4700ef21ffbca
Charles Pernet <charlespernet@gmail.com>
2018-08-22 16:16:18 +0200

    Refacto data for select

--- app/controllers/pro/pages_controller.rb
+++ app/controllers/pro/pages_controller.rb
@@ -3,6 +3,18 @@ module Pro
     def home
       @quote = Quote.new
       @cookoons = Cookoon.random.limit(3).decorate
+      @durations = build_duration
+    end
+
+    private
+
+    def build_duration
+      [
+        {data: 2, display: 'une réunion (2 heures)'},
+        {data: 4, display: 'un comité (4 heures)'},
+        {data: 5, display: 'demi-journée (5 heures)'},
+        {data: 10, display: 'journée complète (10 heures)'}
+      ]
     end
   end
 end
