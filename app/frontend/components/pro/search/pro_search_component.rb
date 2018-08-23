1c0df4cc24eee42eb9c9748bdf099e36997b0bed
Charles Pernet <charlespernet@gmail.com>
2018-08-23 17:07:36 +0200

    Add data variables in Search Component instead of Controller

--- app/frontend/components/pro/search/pro_search_component.rb
+++ app/frontend/components/pro/search/pro_search_component.rb
@@ -2,4 +2,17 @@
 
 module ProSearchComponent
   extend ComponentHelper
+
+  def durations
+    [
+      {data: 2, display: 'une réunion (2 heures)'},
+      {data: 4, display: 'un comité (4 heures)'},
+      {data: 5, display: 'demi-journée (5 heures)'},
+      {data: 10, display: 'journée complète (10 heures)'}
+    ]
+  end
+
+  def people_counts
+    (2..20)
+  end
 end
