7b0f1a545df641d863723aecc7e0d2cee900d5da
Charles Pernet <charlespernet@gmail.com>
2018-09-03 15:03:14 +0200

    change Pro::Reservation#assign_prices

--- app/models/pro/reservation.rb
+++ app/models/pro/reservation.rb
@@ -11,8 +11,12 @@ module Pro
              foreign_key: :pro_reservation_id, dependent: :destroy
 
     monetize :cookoon_price_cents
+    monetize :cookoon_fee_cents
+    monetize :cookoon_fee_tax_cents
     monetize :services_price_cents
-    monetize :fee_cents
+    monetize :services_fee_cents
+    monetize :services_tax_cents
+    monetize :price_excluding_tax_cents
     monetize :price_cents
 
     enum status: %i[draft proposed modification_requested accepted cancelled ongoing passed dead]
@@ -30,13 +34,32 @@ module Pro
 
     private
 
+    DEGRESSION_RATES = {
+      2 => 1,
+      3 => 1,
+      4 => 1,
+      5 => 0.85,
+      6 => 0.85,
+      7 => 0.85,
+      8 => 0.85,
+      9 => 0.85,
+      10 => 0.8
+    }.freeze
+
+    DEFAULTS = {
+      fee_rate: 0.07,
+      tax_rate: 0.2
+    }.freeze
+
     def assign_prices
-      assign_attributes(
-        cookoon_price: duration * cookoon.price,
-        services_price_cents: services.sum(:price_cents),
-        fee: (cookoon_price + services_price) * 0.07,
-        price: cookoon_price + services_price + fee
-      )
+      self.cookoon_price = (duration * cookoon.price) * (DEGRESSION_RATES[duration] || 1)
+      self.cookoon_fee = cookoon_price * DEFAULTS[:fee_rate]
+      self.cookoon_fee_tax = cookoon_fee * DEFAULTS[:tax_rate]
+      self.services_price_cents = services.sum(:price_cents)
+      self.services_fee = services_price * DEFAULTS[:fee_rate]
+      self.services_tax = (services_price + services_fee) * DEFAULTS[:tax_rate]
+      self.price_excluding_tax = cookoon_price + cookoon_fee + services_price + services_fee
+      self.price = price_excluding_tax + cookoon_fee_tax + services_tax
     end
 
     def update_quote_status
