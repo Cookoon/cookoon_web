0362e093c660e16842f2779658fae55223a87fd4
Francois Catuhe <fcatuhe@gmail.com>
2018-09-05 11:42:41 +0200

    include datetime helpers in ReservationMailer

--- app/mailers/pro/reservation_mailer.rb
+++ app/mailers/pro/reservation_mailer.rb
@@ -1,9 +1,12 @@
 module Pro
   class ReservationMailer < ApplicationMailer
+    include DatetimeHelper
+    helper :datetime
+
     def proposed(reservation)
       @reservation = reservation
       @tenant = @reservation.quote.user
-      mail(to: @tenant.full_email, subject: 'COOKOON · Votre proposition')
+      mail(to: @tenant.full_email, subject: 'Votre devis Cookoon est disponible !')
     end
   end
 end
