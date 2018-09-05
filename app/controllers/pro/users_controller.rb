d69243d84085ffef0dca0a01b4e558c4ef35d360
Charles Pernet <charlespernet@gmail.com>
2018-09-05 16:45:50 +0200

    Allow user to edit profil without password and keep possibility to edit password

--- app/controllers/pro/users_controller.rb
+++ app/controllers/pro/users_controller.rb
@@ -6,6 +6,14 @@ module Pro
     end
 
     def update
+      if @user.update(user_params)
+        flash.notice = 'Votre profil a été mis à jour'
+        bypass_sign_in current_user
+        redirect_to edit_pro_user_path
+      else
+        flash.alert = "Votre profil n'a pas pu être sauvegardé"
+        render :edit
+      end
     end
 
     private
@@ -13,6 +21,14 @@ module Pro
     def set_user
       @user = current_user
       authorize @user
+    end
+
+    def user_params
+      if params[:user][:password].blank? && params[:user][:password_confirmation].blank?
+        params[:user].delete(:password)
+        params[:user].delete(:password_confirmation)
+      end
+      params.require(:user).permit(:first_name, :last_name, :email, :phone_number, :password, :password_confirmation)
     end
   end
 end
