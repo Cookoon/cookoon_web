23cb91c89c7d95118c0090feb41f3203f8a27d71
Charles Pernet <charlespernet@gmail.com>
2018-09-05 10:58:26 +0200

    set and authorize user in controller

--- app/controllers/pro/users_controller.rb
+++ app/controllers/pro/users_controller.rb
@@ -1,9 +1,18 @@
 module Pro
   class UsersController < ApplicationController
+    before_action :set_user, only: %i[edit update]
+
     def edit
     end
 
     def update
+    end
+
+    private
+
+    def set_user
+      @user = current_user
+      authorize @user
     end
   end
 end
