adcb412c5f3d9adeb3e4a7350cea57f7473e564b
Francois Catuhe <fcatuhe@gmail.com>
2018-09-05 11:32:33 +0200

    include datetime helpers in Pro::QuoteMailer

--- app/mailers/pro/quote_mailer.rb
+++ app/mailers/pro/quote_mailer.rb
@@ -1,9 +1,12 @@
 module Pro
   class QuoteMailer < ApplicationMailer
+    include DatetimeHelper
+    helper :datetime
+
     def requested(quote)
       @quote = quote
       @tenant = @quote.user
-      mail(to: @tenant.full_email, subject: 'COOKOON · Votre demande de devis')
+      mail(to: @tenant.full_email, subject: 'Vous avez demandé un Cookoon pour votre évènement !')
     end
   end
 end
