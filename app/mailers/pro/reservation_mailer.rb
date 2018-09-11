9f3304e6f9227d47b7bfd0b8014693c03b6b78a5
Francois Catuhe <fcatuhe@gmail.com>
2018-09-11 16:57:07 +0200

    add accepted_to_host mailer

--- app/mailers/pro/reservation_mailer.rb
+++ app/mailers/pro/reservation_mailer.rb
@@ -21,5 +21,19 @@ module Pro
 
       mail(to: @tenant.full_email, subject: 'Votre évènement Cookoon est confirmé !')
     end
+
+    def accepted_to_host(reservation)
+      @reservation = reservation
+      @cookoon = @reservation.cookoon
+      @host = @cookoon.user
+
+      attachments[@reservation.ical_file_name] = {
+        mime_type: 'application/ics',
+        content: @reservation.ical_for(:host).to_ical
+      }
+
+      mail(to: @host.full_email, subject: "L'évènement organisé dans votre Cookoon est confirmé !")
+
+    end
   end
 end
