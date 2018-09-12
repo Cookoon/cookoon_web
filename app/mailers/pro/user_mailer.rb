a55657613780824fba95e1f6ba241434cfb275fb
Charles Pernet <charlespernet@gmail.com>
2018-09-12 16:46:43 +0200

    Change mail subject

--- app/mailers/pro/user_mailer.rb
+++ app/mailers/pro/user_mailer.rb
@@ -3,7 +3,7 @@ module Pro
     def invitation_instructions(user, token)
       @user = user
       @token = token
-      mail(to: @user.full_email, subject: 'Votre invitation pro')
+      mail(to: @user.full_email, subject: 'Vous avez été invité à rejoindre Cookoon')
     end
   end
 end
