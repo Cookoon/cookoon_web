eb617148d596a3ac3550b1e74d2d0463c25ba031
Francois Catuhe <fcatuhe@gmail.com>
2018-10-01 17:12:18 +0200

    add wkhtmltopdf-heroku for staging and production

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -8,7 +8,8 @@
 #
 # https://github.com/mileszs/wicked_pdf/blob/master/README.md
 
-WickedPdf.config = {
+WickedPdf.config ||= {}
+WickedPdf.config.merge!(
   # Path to the wkhtmltopdf executable: This usually isn't needed if using
   # one of the wkhtmltopdf-binary family of gems.
   # exe_path: '/usr/local/bin/wkhtmltopdf',
@@ -19,4 +20,4 @@ WickedPdf.config = {
   # (but can be overridden in `render :pdf` calls)
   layout: 'pro/pdf',
   dpi: '416'
-}
+)
