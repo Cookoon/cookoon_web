6a333c750643ae5614dd8ab09ab836b99f7af036
Francois Catuhe <fcatuhe@gmail.com>
2018-10-02 11:44:10 +0200

    simplify wicked_pdf config

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -8,8 +8,7 @@
 #
 # https://github.com/mileszs/wicked_pdf/blob/master/README.md
 
-WickedPdf.config ||= {}
-WickedPdf.config.merge!(
+WickedPdf.config = {
   # Path to the wkhtmltopdf executable: This usually isn't needed if using
   # one of the wkhtmltopdf-binary family of gems.
   # exe_path: '/usr/local/bin/wkhtmltopdf',
@@ -20,4 +19,4 @@ WickedPdf.config.merge!(
   # (but can be overridden in `render :pdf` calls)
   layout: 'pro/pdf',
   zoom: '4'
-)
+}
