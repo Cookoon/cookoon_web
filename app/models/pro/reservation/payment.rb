0d7329eec1b4b17545062d600a7654e110866568
Charles Pernet <charlespernet@gmail.com>
2018-11-13 16:52:47 +0100

    WIP Pro::Reservation::Payment

--- app/models/pro/reservation/payment.rb
+++ app/models/pro/reservation/payment.rb
@@ -4,11 +4,20 @@ module Pro
       include Stripe::Transferable
       alias_method :reservation, :payable
       # include Stripe::SepaCreditable
-      
+
+      def proceed
+        create_source
+        add_source
+      end
+
       private
 
       def should_capture?
         true
+      end
+
+      def charge_amount_cents
+        price_cents
       end
     end
   end
