a772ed3a312b666feda3db1505084877396d7843
Charles Pernet <charlespernet@gmail.com>
2018-11-15 16:41:41 +0100

    Add referent_email to companies

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_15_084624) do
+ActiveRecord::Schema.define(version: 2018_11_15_145330) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 2018_11_15_084624) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "stripe_customer_id"
+    t.string "referent_email"
   end
 
   create_table "cookoon_searches", force: :cascade do |t|
