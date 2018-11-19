37558510c9505ac567b8429beb28694ff1509755
Charles Pernet <charlespernet@gmail.com>
2018-11-19 17:41:51 +0100

    Fix Company factory to skip after_create callback

--- spec/factories/companies.rb
+++ spec/factories/companies.rb
@@ -2,6 +2,13 @@ FactoryBot.define do
   factory :company do
     name 'Réceptions nouvelles'
     address '12 rue lincoln - 75008 Paris'
+    referent_email 'quentin@cookoon.fr'
+
+    after(:build) { |company| company.class.skip_callback(:create, :after, :stripe_prepare, raise: false) }
+
+    factory :with_stripe_prepare do
+      after(:create) { |company| company.send(:stripe_prepare) }
+    end
 
     trait :with_valid_siren do
       siren '821 316 239'
