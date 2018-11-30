1e36a5755785d2e4a847e3def54d1a2ba965d258
Charles Pernet <charlespernet@gmail.com>
2018-11-30 11:32:59 +0100

    Add basic test for unauthenticated homepage

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -4,21 +4,34 @@ RSpec.feature "Static Pages", type: :feature do
   let(:user) { create(:user) }
 
   describe 'Home pages' do
-    before(:each) do
-      login_as(user, :scope => :user)
+    context 'user is logged in' do
+      before(:each) do
+        login_as(user, :scope => :user)
+      end
+
+      scenario 'user visits root path' do
+        create(:cookoon, :approved)
+        visit root_path
+        expect(page).to have_current_path '/'
+        expect(page).to have_css 'i.co-logo'
+        expect(page).to have_content 'Les derniers Cookoons'
+      end
+
+      scenario 'user visits pro root path' do
+        visit pro_root_path
+        expect(page).to have_current_path '/pro'
+        expect(page).to have_content 'Organisez une expérience originale'
+      end
     end
 
-    scenario 'user visits b2c homepage' do
-      create(:cookoon, :approved)
-      visit root_path
-      expect(page).to have_current_path '/'
-      expect(page).to have_css 'i.co-logo'
+    context 'user is not logged in' do
+      scenario 'user visits root path' do
+        visit root_path
+        expect(page).to have_current_path '/'
+        expect(page).to have_css 'i.co-logo'
+        expect(page).to have_content 'Cookoon est une communauté sélective '
+      end
     end
 
-    scenario 'user visits b2b homepage' do
-      visit pro_root_path
-      expect(page).to have_current_path '/pro'
-      expect(page).to have_content 'Organisez une expérience originale'
-    end
   end
 end
