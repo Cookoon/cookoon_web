904fbe21031a0567e5a66ad2a797cfb71528958b
Charles Pernet <charlespernet@gmail.com>
2018-12-14 16:00:12 +0100

    Rewrite PriceComputer to match new naming conventation for Reservation prices

--- app/models/concerns/pro/reservation/price_computer.rb
+++ app/models/concerns/pro/reservation/price_computer.rb
@@ -6,16 +6,14 @@ module Pro
       def computed_price_attributes
         {
           cookoon_price: compute_degressive_cookoon_price,
-          cookoon_fee: compute_cookoon_fee_including_tax,
+          cookoon_fee: compute_cookoon_fee,
           cookoon_fee_tax: compute_cookoon_fee_tax,
-          services_price_without_tax_and_fee: compute_services_price_without_tax_and_fee,
-          services_fee: compute_services_fee,
+          services_price: compute_services_price,
           services_tax: compute_services_tax,
-          services_price_with_fee: compute_services_price_with_fee,
-          services_price_full: compute_services_price_full,
-          price_excluding_tax: compute_price_excluding_tax,
-          tax_total: compute_tax_total,
-          price: compute_price
+          services_full_price: compute_services_full_price,
+          total_price: compute_total_price,
+          total_tax: compute_total_tax,
+          total_full_price: compute_total_full_price
         }
       end
 
@@ -29,20 +27,20 @@ module Pro
         self.class::DEFAULTS
       end
 
-      def compute_full_cookoon_price
+      def compute_cookoon_price
         duration * cookoon.price
       end
 
       def compute_degressive_cookoon_price
         degression_rate = degression_rates[duration] || 1
-        compute_full_cookoon_price * degression_rate
+        compute_cookoon_price * degression_rate
       end
 
       def compute_cookoon_fee_minus_tax
         compute_degressive_cookoon_price * defaults[:fee_rate] / (1 + defaults[:tax_rate])
       end
 
-      def compute_cookoon_fee_including_tax
+      def compute_cookoon_fee
         compute_degressive_cookoon_price * defaults[:fee_rate]
       end
 
@@ -50,36 +48,28 @@ module Pro
         compute_cookoon_fee_minus_tax * defaults[:tax_rate]
       end
 
-      def compute_services_price_without_tax_and_fee
+      def compute_services_price
         services_price_cents = services.sum(:price_cents)
         Money.new(services_price_cents)
       end
 
-      def compute_services_fee
-        compute_services_price_without_tax_and_fee * defaults[:fee_rate]
-      end
-
-      def compute_services_price_with_fee
-        compute_services_price_without_tax_and_fee + compute_services_fee
-      end
-
       def compute_services_tax
-        (compute_services_price_without_tax_and_fee + compute_services_fee) * defaults[:tax_rate]
+        compute_services_price * defaults[:tax_rate]
       end
 
-      def compute_services_price_full
-        [compute_services_price_without_tax_and_fee, compute_services_fee, compute_services_tax].sum
+      def compute_services_full_price
+        [compute_services_price, compute_services_tax].sum
       end
 
-      def compute_price_excluding_tax
-        [compute_degressive_cookoon_price, compute_cookoon_fee_minus_tax, compute_services_price_without_tax_and_fee, compute_services_fee].sum
+      def compute_total_price
+        [compute_degressive_cookoon_price, compute_cookoon_fee_minus_tax, compute_services_price].sum
       end
 
-      def compute_price
-        [compute_price_excluding_tax, compute_cookoon_fee_tax, compute_services_tax].sum
+      def compute_total_full_price
+        [compute_total_price, compute_cookoon_fee_tax, compute_services_tax].sum
       end
 
-      def compute_tax_total
+      def compute_total_tax
         [compute_services_tax, compute_cookoon_fee_tax].sum
       end
     end
