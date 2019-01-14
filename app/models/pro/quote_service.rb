816d23dbee0f39b40f0f13f2b3fe151a3bb0d164
Charles Pernet <charlespernet@gmail.com>
2019-01-14 10:19:56 +0100

    Add #estimated_price_cents and #unit_price_cents to Pro::QuoteService

--- app/models/pro/quote_service.rb
+++ app/models/pro/quote_service.rb
@@ -5,5 +5,14 @@ module Pro
     enum category: %i[special catering chef corporate]
 
     validates :quantity, numericality: { only_integer: true, greater_than: 0 }
+
+
+    def estimated_price_cents
+      unit_price_cents * quantity
+    end
+
+    def unit_price_cents
+      ::Service::PRICES.try(:[], category.to_sym).try(:[], :unit_price)
+    end
   end
 end
