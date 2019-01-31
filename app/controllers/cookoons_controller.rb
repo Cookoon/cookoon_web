7e8ac67d11f24b9d80d53ee447c9fd2423623813
Charles Pernet <charlespernet@gmail.com>
2019-01-31 18:15:56 +0100

    CookoonsController#show eager load + decorate

--- app/controllers/cookoons_controller.rb
+++ app/controllers/cookoons_controller.rb
@@ -21,7 +21,7 @@ class CookoonsController < ApplicationController
   end
 
   def show
-    @cookoon = Cookoon.find(params[:id])
+    @cookoon = Cookoon.includes(:perks).find(params[:id]).decorate
     authorize @cookoon
 
     params_from_search = @search.to_reservation_attributes.merge(cookoon: @cookoon)
