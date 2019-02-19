f7f013de9a665a42736224f05af91a0ae0aa6d90
Charles Pernet <charlespernet@gmail.com>
2019-02-19 14:59:32 +0100

    Add constant and scopes to categorize Perks

--- app/models/perk.rb
+++ app/models/perk.rb
@@ -3,4 +3,8 @@ class Perk < ApplicationRecord
   belongs_to :perk_specification
 
   delegate :name, :icon_name, to: :perk_specification, allow_nil: true
+
+  scope :equipements, -> { includes(:perk_specification).where(perk_specifications: { name: PerkSpecification::EQUIPEMENTS }) }
+  scope :access, -> { includes(:perk_specification).where(perk_specifications: { name: PerkSpecification::ACCESS }) }
+  scope :unusuals, -> { includes(:perk_specification).where(perk_specifications: { name: PerkSpecification::UNUSUALS }) }
 end
--- app/models/perk_specification.rb
+++ app/models/perk_specification.rb
@@ -4,4 +4,8 @@ class PerkSpecification < ApplicationRecord
   validates :name, uniqueness: true
 
   has_many :perks, dependent: :destroy
+
+  EQUIPEMENTS = ['Écran', 'Système son', 'Chromecast', "Cuisine équipée", 'Apple TV']
+  ACCESS = ['Accessible handicapé', 'Ascenseur']
+  UNUSUALS = ["Jacuzzi", "Sauna", "Rooftop", "Piscine", "Oeuvres d'art", "Barbecue", "Cheminée"]
 end
