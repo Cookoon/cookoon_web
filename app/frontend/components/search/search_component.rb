d0b3a7f17252b585771e9d4ba49af7c42c575b34
Charles Pernet <charlespernet@gmail.com>
2019-05-10 18:26:29 +0200

    Aasm setup + Stimulus Controller for search

--- app/frontend/components/search/search_component.rb
+++ app/frontend/components/search/search_component.rb
@@ -1,3 +1,16 @@
 module SearchComponent
   extend ComponentHelper
+
+  def durations
+    [
+      {data: 4, display: 'Brunch'},
+      {data: 5, display: 'Déjeuner'},
+      {data: 7, display: 'Dîner'},
+      {data: 7, display: 'Cocktail'}
+    ]
+  end
+
+  def people_counts
+    (2..20)
+  end
 end
