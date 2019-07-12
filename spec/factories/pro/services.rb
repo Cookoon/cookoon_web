bef771a7c7e871c8611d47a8390cf991f673cd51
Charles Pernet <charlespernet@gmail.com>
2019-07-12 16:45:50 +0200

    Remove pro:: Tests and move price_computer_spec under global::

--- spec/factories/pro/services.rb
+++
@@ -1,8 +0,0 @@
-FactoryBot.define do
-  factory :pro_service, class: Pro::Service do
-    name { 'Restauration' }
-    quantity { 1 }
-    association :reservation, factory: :pro_reservation
-    unit_price_cents { 10000 }
-  end
-end
