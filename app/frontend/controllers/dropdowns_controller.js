85f62cb761d5368172d535e987d78c723719c913
Charles Pernet <charlespernet@gmail.com>
2019-07-16 12:42:07 +0200

    Fix Dropdown Menu

--- app/frontend/controllers/dropdowns_controller.js
+++ app/frontend/controllers/dropdowns_controller.js
@@ -3,16 +3,12 @@ import { Controller } from 'stimulus'
 export default class extends Controller {
   static targets = ['body', 'trigger'];
 
-  connect() {
-    // this.close()
-  }
-
   toggle() {
     this.bodyTarget.classList.toggle('navigation-dropdown-menu--opened')
   }
 
   close() {
-    if (event.target !== this.triggerTarget) {
+    if (!this.triggerTargets.includes(event.target)) {
       this.bodyTarget.classList.remove('navigation-dropdown-menu--opened')
     }
   }
