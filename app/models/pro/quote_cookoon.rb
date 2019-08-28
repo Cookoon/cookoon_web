969eb224e6a71e67dd9d06873a39e63228e4ff00
Charles Pernet <charlespernet@gmail.com>
2019-08-28 11:33:56 +0200

    Remove pro models

--- app/models/pro/quote_cookoon.rb
+++
@@ -1,6 +0,0 @@
-module Pro
-  class QuoteCookoon < ApplicationRecord
-    belongs_to :quote, class_name: 'Pro::Quote', foreign_key: :pro_quote_id
-    belongs_to :cookoon
-  end
-end
--- app/models/pro/service_specification.rb
+++
@@ -1,7 +0,0 @@
-module Pro
-  class ServiceSpecification < ApplicationRecord
-    validates :name, presence: true
-
-    monetize :unit_price_cents
-  end
-end
