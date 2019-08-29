0bdd0df6ae58353853662fe90d479471cb611b07
Charles Pernet <charlespernet@gmail.com>
2019-08-29 17:22:05 +0200

    add cancel event to aasm

--- app/jobs/reservations_cleanup_job.rb
+++ app/jobs/reservations_cleanup_job.rb
@@ -10,19 +10,19 @@ class ReservationsCleanupJob < ApplicationJob
   private
 
   def cleanup_dropped_before_payment
-    Reservation.dropped_before_payment.each(&:dead!)
+    Reservation.dropped_before_payment.each(&:kill!)
   end
 
   def cleanup_short_notice
     Reservation.short_notice.each do |reservation|
-      reservation.cancelled!
+      reservation.cancel!
       ReservationMailer.autocancel_short_notice_to_tenant(reservation).deliver_later
     end
   end
 
   def cleanup_stripe_will_not_capture
     Reservation.stripe_will_not_capture.each do |reservation|
-      reservation.cancelled!
+      reservation.cancel!
       ReservationMailer.autocancel_stripe_period_to_tenant(reservation).deliver_later
       ReservationMailer.autocancel_stripe_period_to_host(reservation).deliver_later
     end
