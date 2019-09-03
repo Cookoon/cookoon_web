c7d383072ca0783e3afbf72d56f4bc2854f8eae4
Charles Pernet <charlespernet@gmail.com>
2019-09-03 18:14:07 +0200

    Improvement 3sept

--- app/decorators/reservation_decorator.rb
+++ app/decorators/reservation_decorator.rb
@@ -2,7 +2,7 @@ class ReservationDecorator < Draper::Decorator
   delegate_all
 
   def recap_string
-    "Votre maitre d'hotêl vous acceuillera le #{start_at.day} #{I18n.t('date.month_names')[start_at.month]} pour un #{type_name} à #{start_at.strftime('%HH%M')} avec #{people_count_text}"
+    "Votre maitre d'hôtel vous accueillera le #{start_at.day} #{I18n.t('date.month_names')[start_at.month]} pour un #{type_name} à #{start_at.strftime('%HH%M')} avec #{people_count_text}"
   end
 
   def default_people_count
