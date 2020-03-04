49ac4eaed0ecd91de7485f0e2a6b97d5c3144752
Alice Fabre <alice.fabre@hotmail.fr>
2020-03-04 15:35:40 +0100

    redirection set to the old one (edit_users_path) after create stripe account + link stripe bank account // identity documents will be posted to Stripe by back-office for the moment

--- app/controllers/stripe_accounts_controller.rb
+++ app/controllers/stripe_accounts_controller.rb
@@ -6,8 +6,9 @@ class StripeAccountsController < ApplicationController
 
   def create
     if @service.create_and_link_account
+      # redirect_to "#{@service.stripe_account_link_id_verification.url}"
       flash[:notice] = 'Votre compte est maintenant lié avec Stripe. Vous pourrez prochainement récupérer vos paiements en attente'
-      redirect_to "#{@service.stripe_account_link_id_verification.url}"
+      redirect_to edit_users_path
     else
       flash.now[:alert] = @service.error_messages
       render :new
@@ -21,6 +22,17 @@ class StripeAccountsController < ApplicationController
     #   render :new
     # end
   end
+
+  # # To implement later
+  # def add_identity_documents
+  #   @service = StripeAccountService.new(user: current_user)
+  #   if @service.add_identity_documents_for_existing_accounts
+  #     redirect_to "#{@service.stripe_account_link_id_verification.url}"
+  #     flash[:notice] = 'Vos documents ont bien été transmis à Stripe. Vous pourrez après validation de leur part récupérer vos paiements en attente.'
+  #   else
+  #     flash.now[:alert] = @service.error_messages
+  #   end
+  # end
 
   private
 
