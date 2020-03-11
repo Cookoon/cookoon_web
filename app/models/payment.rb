c0d58a4ee27512f3cb3b12635a6d840f5a5806a0
Alice Fabre <alice.fabre@hotmail.fr>
2020-03-11 11:45:16 +0100

    replace stripe_charge methods by  stripe_payment_intent methods

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -11,19 +11,31 @@ class Payment
     @errors = []
   end
 
-  def proceed
-    before_proceed
-    create_stripe_charge if charge_needed?
-    after_proceed
+  def create_or_retrieve_and_update
+    payable.stripe_charge_id.nil? ? create : retrieve_and_update
   end
 
-  def refund
-    refund_stripe_charge
+  def create
+    create_stripe_intent if charge_needed?
+    # after_create
+  end
+  # def proceed
+  #   before_proceed
+  #   create_stripe_charge if charge_needed?
+  #   after_proceed
+  # end
+
+  def retrieve_and_update
+    retrieve_and_update_stripe_intent
   end
 
-  def capture
-    capture_stripe_charge
-  end
+  # def refund
+  #   refund_stripe_charge
+  # end
+
+  # def capture
+  #   capture_stripe_charge
+  # end
 
   def displayable_errors
     if errors.any?
@@ -35,30 +47,97 @@ class Payment
 
   private
 
-  def before_proceed; end
+  # def before_proceed; end
 
-  def after_proceed; end
+  # def after_proceed; end
 
   def charge_needed?
     charge_amount_cents.positive?
   end
 
-  def charge_amount_cents
-    # could raise a NotImplementedError because should never be called
-    # Each specific Payment Class needs to implement this method
-    payment_amount_cents
-  end
+  # def charge_amount_cents
+  #   # could raise a NotImplementedError because should never be called
+  #   # Each specific Payment Class needs to implement this method
+  #   payment_amount_cents
+  # end
 
-  def charge_metadata
-    {}
-  end
+  # def charge_metadata
+  #   {}
+  # end
 
-  def transfer_metadata
-    {}
-  end
+  # def transfer_metadata
+  #   {}
+  # end
 
   def stripe_customer
     return nil unless user.stripe_customer?
     user.stripe_customer
   end
 end
+
+
+# # Code Charles
+# class Payment
+#   include Stripe::Chargeable
+
+#   attr_reader :payable, :options, :errors
+
+#   delegate :user, :payment_amount_cents, to: :payable
+
+#   def initialize(payable, options = {})
+#     @payable = payable
+#     @options = options
+#     @errors = []
+#   end
+
+#   def proceed
+#     before_proceed
+#     create_stripe_charge if charge_needed?
+#     after_proceed
+#   end
+
+#   def refund
+#     refund_stripe_charge
+#   end
+
+#   def capture
+#     capture_stripe_charge
+#   end
+
+#   def displayable_errors
+#     if errors.any?
+#       errors.join(' ')
+#     else
+#       "Une erreur est survenue avec notre prestataire de paiement, essayez à nouveau ou contactez notre service d'aide"
+#     end
+#   end
+
+#   private
+
+#   def before_proceed; end
+
+#   def after_proceed; end
+
+#   def charge_needed?
+#     charge_amount_cents.positive?
+#   end
+
+#   def charge_amount_cents
+#     # could raise a NotImplementedError because should never be called
+#     # Each specific Payment Class needs to implement this method
+#     payment_amount_cents
+#   end
+
+#   def charge_metadata
+#     {}
+#   end
+
+#   def transfer_metadata
+#     {}
+#   end
+
+#   def stripe_customer
+#     return nil unless user.stripe_customer?
+#     user.stripe_customer
+#   end
+# end
--- app/models/reservation/payment.rb
+++ app/models/reservation/payment.rb
@@ -3,25 +3,25 @@ class Reservation
     include Stripe::Transferable
     alias_method :reservation, :payable
 
-    private
+   private
 
-    def after_proceed
-      reservation.charge! if errors.empty?
-    end
+    # def after_proceed
+    #   reservation.charge! if errors.empty?
+    # end
 
-    def should_capture?
-      ActiveModel::Type::Boolean.new.cast(options[:capture]) || false
-    end
+    # def should_capture?
+    #   ActiveModel::Type::Boolean.new.cast(options[:capture]) || false
+    # end
 
     def charge_amount_cents
       reservation.business? ? reservation.total_price_cents : reservation.total_with_tax_cents
     end
 
-    def charge_description
+    def intent_description
       "Paiement pour #{reservation.cookoon.name}"
     end
 
-    def charge_metadata
+    def intent_metadata
       {
         reservation_id: reservation.id,
         reservation_price: charge_amount_cents,
@@ -30,16 +30,64 @@ class Reservation
       }
     end
 
-    def transfer_metadata
-      {}
-    end
+    # def transfer_metadata
+    #   {}
+    # end
 
-    def transfer_amount
-      reservation.host_payout_price_cents
-    end
+    # def transfer_amount
+    #   reservation.host_payout_price_cents
+    # end
 
-    def transfer_destination
-      reservation.cookoon.user.stripe_account_id
-    end
+    # def transfer_destination
+    #   reservation.cookoon.user.stripe_account_id
+    # end
   end
 end
+
+
+# # Code Charles
+# class Reservation
+#   class Payment < ::Payment
+#     include Stripe::Transferable
+#     alias_method :reservation, :payable
+
+#     private
+
+#     def after_proceed
+#       reservation.charge! if errors.empty?
+#     end
+
+#     def should_capture?
+#       ActiveModel::Type::Boolean.new.cast(options[:capture]) || false
+#     end
+
+#     def charge_amount_cents
+#       reservation.business? ? reservation.total_price_cents : reservation.total_with_tax_cents
+#     end
+
+#     def charge_description
+#       "Paiement pour #{reservation.cookoon.name}"
+#     end
+
+#     def charge_metadata
+#       {
+#         reservation_id: reservation.id,
+#         reservation_price: charge_amount_cents,
+#         reservation_services_price: reservation.services_with_tax_cents,
+#         reservation_services: reservation.services.payment_tied_to_reservation.pluck(:name).join(' · ')
+#       }
+#     end
+
+#     def transfer_metadata
+#       {}
+#     end
+
+#     def transfer_amount
+#       reservation.host_payout_price_cents
+#     end
+
+#     def transfer_destination
+#       reservation.cookoon.user.stripe_account_id
+#     end
+#   end
+# end
