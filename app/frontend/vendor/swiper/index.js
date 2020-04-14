599e8c93795c73718feeab7bb04421f11dae273a
Alice Fabre <alice.fabre@hotmail.fr>
2020-04-14 14:22:51 +0200

    add Autoplay module from Swiper

--- app/frontend/vendor/swiper/index.js
+++ app/frontend/vendor/swiper/index.js
@@ -1,8 +1,10 @@
-import { Swiper, Navigation, Pagination } from 'swiper/dist/js/swiper.esm.js';
+import { Swiper, Navigation, Pagination, Autoplay } from 'swiper/dist/js/swiper.esm.js';
+// import { Swiper, Navigation, Pagination } from 'swiper/dist/js/swiper.esm.js';
 import 'swiper/dist/css/swiper.css';
 
 import './swiper.scss';
 
-Swiper.use([Navigation, Pagination]);
+Swiper.use([Navigation, Pagination, Autoplay]);
+// Swiper.use([Navigation, Pagination]);
 
 export default Swiper;
