1ee4510c826d581acfe09bfba586df33ae70e3fd
Alice Fabre <alice.fabre@hotmail.fr>
2020-05-13 15:59:56 +0200

    import function that enables to change image when mouse over or mouse out the submit form

--- app/frontend/components/search/search.js
+++ app/frontend/components/search/search.js
@@ -1,6 +1,8 @@
 import application from "stimulus_application";
 import { definitionsFromContext } from "stimulus/webpack-helpers";
 import "./search.scss";
+import {changeImageSubmitTag} from "./change_image_submit_tag.js";
+changeImageSubmitTag();
 
 const context = require.context('./', true, /_controller\.js$/);
 application.load(definitionsFromContext(context));
