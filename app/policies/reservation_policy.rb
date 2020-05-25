7efc218c0bcf595ca3df64e65fd4a82bee0082c5
Alice Fabre <alice.fabre@hotmail.fr>
2020-05-25 16:55:24 +0200

    add policy for secret_services

--- app/policies/reservation_policy.rb
+++ app/policies/reservation_policy.rb
@@ -44,6 +44,10 @@ class ReservationPolicy < ApplicationPolicy
     record.user == user
   end
 
+  def secret_services?
+    secret?
+  end
+
   class Scope < Scope
     def resolve
       scope.for_tenant(user).displayable
