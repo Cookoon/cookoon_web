5253bbec023bb16ac8335183c1730f8aae088445
Alice Fabre <alice.fabre@hotmail.fr>
2020-05-28 20:48:03 +0200

    add controller for admin/reservations

---
+++ app/controllers/admin/reservations_controller.rb
@@ -0,0 +1,29 @@
+module Admin
+  class ReservationsController < ApplicationController
+    # not necessary because it is specified directly in routes
+    # before_action :require_admin
+    before_action :find_reservation, only: %i[show]
+
+    def index
+      @reservations = policy_scope([:admin, Reservation]).includes(:cookoon, :user, cookoon: [:user])
+      @reservations_charged = @reservations.charged.decorate
+      @reservations_accepted = @reservations.accepted.decorate
+      @reservations_all = @reservations.decorate
+    end
+
+    def show
+    end
+
+    private
+
+    def find_reservation
+      @reservation = Reservation.find(params[:id]).decorate
+      authorize @reservation
+    end
+
+    # def require_admin
+    #   current_user.admin == true
+    # end
+
+  end
+end
