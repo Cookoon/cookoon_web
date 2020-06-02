1058ceddb7728601439c1f3f23a386c1546b695b
Alice Fabre <alice.fabre@hotmail.fr>
2020-06-02 15:22:34 +0200

    add column cookoon_butler_price_cents to reservations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_22_200906) do
+ActiveRecord::Schema.define(version: 2020_06_02_131827) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -160,6 +160,7 @@ ActiveRecord::Schema.define(version: 2020_05_22_200906) do
     t.integer "total_tax_cents", default: 0, null: false
     t.integer "total_with_tax_cents", default: 0, null: false
     t.string "stripe_services_id"
+    t.integer "cookoon_butler_price_cents", default: 0, null: false
     t.index ["cookoon_id"], name: "index_reservations_on_cookoon_id"
     t.index ["menu_id"], name: "index_reservations_on_menu_id"
     t.index ["user_id"], name: "index_reservations_on_user_id"
