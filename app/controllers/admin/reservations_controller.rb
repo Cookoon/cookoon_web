565cfb5449c7d7088a118596e1159adb4f16b179
Alice Fabre <alice.fabre@hotmail.fr>
2020-06-06 18:42:02 +0200

    add #require_payment_for_menu

--- app/controllers/admin/reservations_controller.rb
+++ app/controllers/admin/reservations_controller.rb
@@ -2,15 +2,26 @@ module Admin
   class ReservationsController < ApplicationController
     # not necessary because it is specified directly in routes
     # before_action :require_admin
-    before_action :find_reservation, only: %i[show]
+    before_action :find_reservation, only: %i[show require_payment_for_menu]
 
     def index
-      @reservations = policy_scope([:admin, Reservation]).includes(:cookoon, :user, :menu, cookoon: [:user])
+      @reservations = policy_scope([:admin, Reservation]).includes(:cookoon, :user, :menu, cookoon: [:user]).order(id: :desc)
       @reservations_charged = @reservations.charged.decorate
       @reservations_accepted = @reservations.accepted.decorate
     end
 
     def show
+    end
+
+    def require_payment_for_menu
+      if @reservation.update(menu_payment_status: "payment_required")
+        flash.notice = "Le statut de la réservation a bien été mis à jour"
+        # TO DO send email to user to require payment
+        render :show
+      else
+        flash.alert = "Il y a eu un problème"
+        render :show
+      end
     end
 
     private
