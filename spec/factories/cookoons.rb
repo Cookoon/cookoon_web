aafc5c385d98649b8962fa75b18ea849752fd513
Alice Fabre <alice.fabre@hotmail.fr>
2020-06-09 18:11:35 +0200

    update rspec to be valid (2 photos added instead of one)

--- spec/factories/cookoons.rb
+++ spec/factories/cookoons.rb
@@ -12,12 +12,15 @@ FactoryBot.define do
     category { 'Appartement' }
     photos {
       <<~CLOUDINARY
-        [{\"public_id\":\"tlukyujdrwp7krc6setx\",\"version\":1520416362,\"signature\":\"863d76cc03532aa5201a419b99555bdb10291428\",\"width\":512,\"height\":512,\"format\":\"png\",\"resource_type\":\"image\",\"created_at\":\"2018-03-07T09:52:42Z\",\"tags\":[\"development_env\",\"attachinary_tmp\"],\"bytes\":38308,\"type\":\"upload\",\"etag\":\"e44c8191e4b29efa962ebbb826f0801a\",\"placeholder\":false,\"url\":\"http://res.cloudinary.com/cookoon-dev/image/upload/v1520416362/tlukyujdrwp7krc6setx.png\",\"secure_url\":\"https://res.cloudinary.com/cookoon-dev/image/upload/v1520416362/tlukyujdrwp7krc6setx.png\",\"original_filename\":\"ic_launcher-web\"}]
+        [
+          {\"public_id\":\"tlukyujdrwp7krc6setx\",\"version\":1520416362,\"signature\":\"863d76cc03532aa5201a419b99555bdb10291428\",\"width\":512,\"height\":512,\"format\":\"png\",\"resource_type\":\"image\",\"created_at\":\"2018-03-07T09:52:42Z\",\"tags\":[\"development_env\",\"attachinary_tmp\"],\"bytes\":38308,\"type\":\"upload\",\"etag\":\"e44c8191e4b29efa962ebbb826f0801a\",\"placeholder\":false,\"url\":\"http://res.cloudinary.com/cookoon-dev/image/upload/v1520416362/tlukyujdrwp7krc6setx.png\",\"secure_url\":\"https://res.cloudinary.com/cookoon-dev/image/upload/v1520416362/tlukyujdrwp7krc6setx.png\",\"original_filename\":\"ic_launcher-web\"},
+          {\"public_id\":\"tlukyujdrwp7krc6setx\",\"version\":1520416362,\"signature\":\"863d76cc03532aa5201a419b99555bdb10291428\",\"width\":512,\"height\":512,\"format\":\"png\",\"resource_type\":\"image\",\"created_at\":\"2018-03-07T09:52:42Z\",\"tags\":[\"development_env\",\"attachinary_tmp\"],\"bytes\":38308,\"type\":\"upload\",\"etag\":\"e44c8191e4b29efa962ebbb826f0801a\",\"placeholder\":false,\"url\":\"http://res.cloudinary.com/cookoon-dev/image/upload/v1520416362/tlukyujdrwp7krc6setx.png\",\"secure_url\":\"https://res.cloudinary.com/cookoon-dev/image/upload/v1520416362/tlukyujdrwp7krc6setx.png\",\"original_filename\":\"ic_launcher-web\"}
+        ]
       CLOUDINARY
     }
 
     trait :with_photos_upload do
-      photo_urls { %w[https://picsum.photos/400/300] }
+      photo_urls { %w[https://picsum.photos/400/300 https://picsum.photos/400/300] }
     end
   end
 end
