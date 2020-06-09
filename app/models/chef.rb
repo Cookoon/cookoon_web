b812a070cb9693350ff41a037b94a2add5b08d86
Alice Fabre <alice.fabre@hotmail.fr>
2020-06-09 15:41:23 +0200

    add validations for base_price and min_price

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -6,7 +6,21 @@ class Chef < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
   validates :photos, presence: true
+  validates :base_price, numericality: { greater_than_or_equal_to: 0 }
+  validates :min_price, numericality: { greater_than_or_equal_to: 0 }
+
+  validate :base_price_or_min_price_positive
 
   monetize :base_price_cents
   monetize :min_price_cents
+
+  def base_price_or_min_price_positive
+    if base_price.positive? && min_price.positive?
+      errors.add(:base_price, "ne peut pas être positif si le prix minimum est positif")
+      errors.add(:min_price, "ne peut pas être positif si le prix de prestation est positif")
+    elsif base_price.zero? && min_price.zero?
+      errors.add(:base_price, "ne peut pas être nul si le prix minimum est nul")
+      errors.add(:min_price, "ne peut pas être nul si le prix de prestation est nul")
+    end
+  end
 end
