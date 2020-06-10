9baca54424f2390ff871f2fee5a30b4c1fc4a418
Alice Fabre <alice.fabre@hotmail.fr>
2020-06-10 14:23:36 +0200

    change category valitions to include good values

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -2,10 +2,10 @@ class Dish < ApplicationRecord
   belongs_to :menu
 
   validates :name, presence: true
-  validates :category, presence: true
+  validates :category, presence: true, inclusion: { in: %w(aperitif amuse_bouches starter course entremets dessert) }
   validates :order, presence: true, numericality: { greater_than: 0 }
 
-  enum category: %i[aperitif amuse_bouches starter course entremets dessert]
+  # enum category: %i[aperitif amuse_bouches starter course entremets dessert]
 
   after_initialize do |dish|
     dish.order = count_dishes + 1
