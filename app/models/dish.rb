92fdf5c84c70799eee57745fe214b40170aeb41b
Alice Fabre <alice.fabre@hotmail.fr>
2020-06-16 16:24:19 +0200

    simplify validation on category

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -2,25 +2,9 @@ class Dish < ApplicationRecord
   belongs_to :menu
 
   validates :name, presence: true
-  validates :category, presence: true, inclusion: { in: %w(aperitif amuse_bouches starter course entremets dessert) }
+  validates :category, presence: true
   validates :order, presence: true, numericality: { greater_than: 0 }
 
-  CATEGORIES = [
-    [ "Canapé", :aperitif ],
-    [ "Amuse-bouches", :amuse_bouches ],
-    [ "Entrée", :starter ],
-    [ "Plat", :course ],
-    [ "Entremets", :entremets ],
-    [ "Dessert", :dessert ],
-  ].freeze
-
-  FRENCH_CATEGORIES = {
-    aperitif: "Canapé",
-    amuse_bouches: "Amuse-bouches",
-    starter: "Entrée",
-    course: "Plat",
-    entremets: "Entremets",
-    dessert: "Dessert",
-  }.freeze
+  CATEGORIES = %w[Apéritif Amuse-bouches Entrée Plat Entremets Dessert].freeze
 
 end
