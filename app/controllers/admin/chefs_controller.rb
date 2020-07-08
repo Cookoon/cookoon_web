4698864ccfe015b93170d6391e8900623305a536
Alice Fabre <alice.fabre@hotmail.fr>
2020-07-08 18:33:46 +0200

    add inclusions on menus

--- app/controllers/admin/chefs_controller.rb
+++ app/controllers/admin/chefs_controller.rb
@@ -9,9 +9,9 @@ module Admin
     end
 
     def show
-      @menus_initial = @chef.menus.where(status: "initial")
-      @menus_active = @chef.menus.where(status: "active")
-      @menus_archived = @chef.menus.where(status: "archived")
+      @menus_initial = @chef.menus.includes(:dishes).where(status: "initial")
+      @menus_active = @chef.menus.includes(:dishes).where(status: "active")
+      @menus_archived = @chef.menus.includes(:dishes).where(status: "archived")
     end
 
     def new
