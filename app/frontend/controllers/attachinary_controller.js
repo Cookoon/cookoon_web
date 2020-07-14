c065571705114aaea17b6dc408b6e7ca01ab49f7
Alice Fabre <alice.fabre@hotmail.fr>
2020-07-15 01:02:49 +0200

    update to treat each attachinary-inputs individually

--- app/frontend/controllers/attachinary_controller.js
+++ app/frontend/controllers/attachinary_controller.js
@@ -9,17 +9,31 @@ export default class extends Controller {
     $('.attachinary-input').attachinary();
 
     // Display upload-zone if attachinary-input is not disabled
-    if (!($('.attachinary-input')[0] && $('.attachinary-input')[0].disabled)) {
-      $('label.attachinary').addClass('upload-zone');
-    }
-
-    // Remove upload-zone if last uploaded picture disables attachinary-input
-    $('.attachinary-input').on('fileuploaddone', function(event) {
-      if (event.result[0].disabled) {
-        $('label.attachinary').removeClass('upload-zone');
+    $('.attachinary-input').each(function() {
+      if (!($(this)[0] && $(this)[0].disabled)) {
+        $("label[for='"+$(this).attr('id')+"']").addClass('upload-zone');
       }
     });
-  
+    // Old code that was ok for only one attachinary input
+    // if (!($('.attachinary-input')[0] && $('.attachinary-input')[0].disabled)) {
+    //   $('label.attachinary').addClass('upload-zone');
+    // }
+
+    // Remove upload-zone if last uploaded picture disables attachinary-input
+    $('.attachinary-input').each(function() {
+      $(this).on('fileuploaddone', function(event) {
+        if (event.result[0].disabled) {
+          $("label[for='"+$(this).attr('id')+"']").removeClass('upload-zone');
+        }
+      });
+    });
+    // Old code that was ok for only one attachinary input
+    // $('.attachinary-input').on('fileuploaddone', function(event) {
+    //   if (event.result[0].disabled) {
+    //     $('label.attachinary').removeClass('upload-zone');
+    //   }
+    // });
+
     $(document).on('attachinary:fileremoved', function() {
       $('label.attachinary').addClass('upload-zone');
     })
