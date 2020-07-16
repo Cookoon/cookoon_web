e412c0d22a85de01f89f48dcca2322e7b4c61bce
Alice Fabre <alice.fabre@hotmail.fr>
2020-07-16 11:14:18 +0200

    add all_photos method

--- app/models/cookoon.rb
+++ app/models/cookoon.rb
@@ -62,6 +62,10 @@ class Cookoon < ApplicationRecord
     perks.map(&:name).join(' / ')
   end
 
+  def all_photos
+    self.photos.shuffle.unshift(self.main_photo, self.long_photo)
+  end
+
   private
 
   def overlapping_reservations(date_range)
