a1e5a5a89393dc28625d82d41e8bebd31ddb2b11
Alice Fabre <alice.fabre@hotmail.fr>
2020-08-25 15:36:49 +0200

    add validate_services and ask_services_payment actions

--- app/controllers/admin/reservations_controller.rb
+++ app/controllers/admin/reservations_controller.rb
@@ -3,7 +3,8 @@ module Admin
     # not necessary because it is specified directly in routes
     # before_action :require_admin
     before_action :find_reservation, only: %i[show]
-    before_action :find_reservation_with_renservation_id, only: %i[ask_menu_payment validate_menu]
+    before_action :find_reservation_with_reservation_id, only: %i[validate_menu ask_menu_payment validate_services ask_services_payment]
+
     def index
       @reservations = policy_scope([:admin, Reservation]).includes(:cookoon, :user, menu: [:chef], cookoon: [:user]).order(id: :desc)
     end
@@ -32,6 +33,27 @@ module Admin
       end
     end
 
+    def validate_services
+      if @reservation.update(services_status: "validated")
+        flash.notice = "Les services ont bien été validés"
+        redirect_to admin_reservation_path(@reservation)
+      else
+        flash.alert = "Il y a eu un problème"
+        render :show
+      end
+    end
+
+    def ask_services_payment
+      if @reservation.update(services_status: "payment_required")
+        flash.notice = "Le paiement des services va bien être demandé"
+        # TO DO send email to user to require payment
+        redirect_to admin_reservation_path(@reservation)
+      else
+        flash.alert = "Il y a eu un problème"
+        render :show
+      end
+    end
+
     private
 
     def find_reservation
@@ -39,7 +61,7 @@ module Admin
       authorize @reservation, policy_class: Admin::ReservationPolicy
     end
 
-    def find_reservation_with_renservation_id
+    def find_reservation_with_reservation_id
       @reservation = Reservation.find(params[:reservation_id]).decorate
       authorize @reservation, policy_class: Admin::ReservationPolicy
     end
