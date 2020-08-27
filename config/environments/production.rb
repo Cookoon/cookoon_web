9f244d7c22e0819c739e8361638221afa11696c0
Alice Fabre <51875307+Alice-F@users.noreply.github.com>
2020-08-27 16:28:53 +0200

    Revert "Revert "change app.cookoon.fr to membre.cookoon.club""

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,8 +67,8 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method     = :postmark
   config.action_mailer.postmark_settings   = { api_token: ENV['POSTMARK_API_KEY'] }
-  config.action_mailer.default_url_options = { host: 'app.cookoon.fr' }
-  config.roadie.url_options = { host: 'app.cookoon.fr', scheme: 'https' }
+  config.action_mailer.default_url_options = { host: 'membre.cookoon.club' }
+  config.roadie.url_options = { host: 'membre.cookoon.club', scheme: 'https' }
   config.action_mailer.asset_host = nil
 
   # Ignore bad email addresses and do not raise email delivery errors.
