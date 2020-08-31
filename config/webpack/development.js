7637e9766938c652d7c69a4b9ffbc819fdf995e6
Alice Fabre <alice.fabre@hotmail.fr>
2020-08-31 21:06:50 +0200

    Preventing Babel from transpiling NodeModules packages only in development

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -5,4 +5,7 @@ const webpackBundleAnalyzerConfig = require('./config/webpackBundleAnalyzer');
 
 environment.config.merge(webpackBundleAnalyzerConfig);
 
+// Preventing Babel from transpiling NodeModules packages
+environment.loaders.delete('nodeModules');
+
 module.exports = environment.toWebpackConfig();
