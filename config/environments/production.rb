6b4656c2ceb6b70dbf5b24502f5c4c5d30cd9617
Alice Fabre <alice.fabre@hotmail.fr>
2020-09-04 10:33:27 +0200

    authorize mailer preview in production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,6 +75,9 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.show_previews = true
+  config.action_mailer.preview_path = "#{Rails.root}/spec/mailers/previews"
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
