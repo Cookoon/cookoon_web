0b25895dde521b240a4f4c117510df84eab965d7
Alice Fabre <alice.fabre@hotmail.fr>
2020-09-06 18:09:28 +0200

    uncomment line to send welcome e-mail

--- app/models/user.rb
+++ app/models/user.rb
@@ -135,7 +135,7 @@ class User < ApplicationRecord
   end
 
   def send_welcome_email
-    # UserMailer.welcome_email(self).deliver_later
+    UserMailer.welcome_email(self).deliver_later
   end
 
   def upsert_mailchimp_subscription
