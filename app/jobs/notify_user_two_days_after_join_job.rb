ea80ffe76af2b2b8807353d5143dd2aea9638aeb
Alice Fabre <alice.fabre@hotmail.fr>
2020-09-09 14:08:36 +0200

    update time

--- app/jobs/notify_user_two_days_after_join_job.rb
+++ app/jobs/notify_user_two_days_after_join_job.rb
@@ -3,7 +3,7 @@ class NotifyUserTwoDaysAfterJoinJob < ApplicationJob
 
   def perform(scheduled_time)
     scheduled_date_time = Time.zone.at(scheduled_time)
-    User.has_no_cookoon.joined_in_day_range_around(scheduled_date_time.ago(1.days)).each do |user|
+    User.has_no_cookoon.joined_in_day_range_around(scheduled_date_time.ago(7.days)).each do |user|
       UserMailer.notify_two_days_after_join(user).deliver_later
     end
   end
