f85f63d0920542450495c0035f06dcad83cb0e07
Alice Fabre <alice.fabre@hotmail.fr>
2020-09-10 10:05:50 +0200

    add meal_type to menu_params

--- app/controllers/admin/menus_controller.rb
+++ app/controllers/admin/menus_controller.rb
@@ -77,7 +77,7 @@ module Admin
     end
 
     def menu_params
-      params.require(:menu).permit(:description, :unit_price, :type)
+      params.require(:menu).permit(:description, :unit_price, :meal_type)
     end
 
     def menu_title_params
