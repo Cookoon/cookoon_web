932ad418dec566a5da2addbb2a9af5d15d4718ff
Alice Fabre <alice.fabre@hotmail.fr>
2020-09-10 10:09:23 +0200

    remove migration files

--- db/migrate/20200910072223_add_type_to_menus.rb
+++
@@ -1,5 +0,0 @@
-class AddTypeToMenus < ActiveRecord::Migration[5.2]
-  def change
-    add_column :menus, :type, :string
-  end
-end
--- db/migrate/20200910075941_remove_type_from_menus.rb
+++
@@ -1,5 +0,0 @@
-class RemoveTypeFromMenus < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :menus, :type, :string
-  end
-end
