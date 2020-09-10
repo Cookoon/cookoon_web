79b7e1cb7f916b044a9257d79e7b80756583fe40
Alice Fabre <alice.fabre@hotmail.fr>
2020-09-10 10:08:11 +0200

    add meal_type validation + values

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,6 +1,7 @@
 class Menu < ApplicationRecord
 
   STATUSES = %w[initial active archived].freeze
+  MEAL_TYPES = %w[seated_meal standing_meal].freeze
   MAX_PER_CHEF = 2
 
   belongs_to :chef
@@ -12,6 +13,7 @@ class Menu < ApplicationRecord
   validates :description, presence: true
   validates :unit_price, presence: true, numericality: { greater_than: 0 }
   validates :status, presence: true, inclusion: { in: Menu::STATUSES }
+  validates :meal_type, presence: true, inclusion: { in: Menu::MEAL_TYPES }
 
   default_scope -> { order(unit_price_cents: :asc) }
   scope :active, -> { where("status = 'active'") }
