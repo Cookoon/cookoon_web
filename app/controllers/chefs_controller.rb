bfc76520df14e8672565de696aa9820e1c8a8bc3
Alice Fabre <alice.fabre@hotmail.fr>
2020-09-10 14:25:59 +0200

    display chefs depending if reservation is seated or standing

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -4,15 +4,16 @@ class ChefsController < ApplicationController
   before_action :find_chef, only: %i[show]
 
   def index
-    # filtering_params = {
-    #   accomodates_for: @reservation.people_count,
-    #   available_in: (@reservation.start_at..@reservation.end_at),
-    #   available_for: current_user
-    # }
-
     # TO DO display only chefs that do not have another reservation
-    # policy_scope displays chef that only have active menus thanks to scope.has_active_menus in chef policy
-    @chefs = policy_scope(Chef).includes(:main_photo_files, :chef_perks, chef_perks: :chef_perk_specification)
+    # # policy_scope displays chef that only have active menus thanks to scope.has_active_menus in chef policy
+    # @chefs = policy_scope(Chef).includes(:main_photo_files, :chef_perks, chef_perks: :chef_perk_specification)
+    if @reservation.seated?
+      @chefs = policy_scope(Chef).has_active_seated_menus.includes(:main_photo_files, :chef_perks, chef_perks: :chef_perk_specification)
+    elsif @reservation.standing?
+      @chefs = policy_scope(Chef).has_active_standing_menus.includes(:main_photo_files, :chef_perks, chef_perks: :chef_perk_specification)
+    else
+      @chefs = policy_scope(Chef).none
+    end
   end
 
   def show
