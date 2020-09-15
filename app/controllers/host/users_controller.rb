17cf56964e6f81834b646e324d5dcc90b354bc7d
Alice Fabre <alice.fabre@hotmail.fr>
2020-09-15 10:54:45 +0200

    correct inclusion (main_photo_files instead of photo_files

--- app/controllers/host/users_controller.rb
+++ app/controllers/host/users_controller.rb
@@ -3,6 +3,6 @@ class Host::UsersController < ApplicationController
     @user = current_user
     authorize [:host, @user]
     @pending_reservations_count = @user.reservation_requests.charged.count
-    @cookoons = @user.cookoons.includes(:photo_files).decorate
+    @cookoons = @user.cookoons.includes(:main_photo_files).decorate
   end
 end
