8efc7c687c41645fd9a34541944a598b9bfae382
Alice Fabre <alice.fabre@hotmail.fr>
2020-09-15 10:49:39 +0200

    keep only show / index / find_cookoon / find_reservation

--- app/controllers/cookoons_controller.rb
+++ app/controllers/cookoons_controller.rb
@@ -2,8 +2,7 @@ class CookoonsController < ApplicationController
   include DatetimeHelper
 
   before_action :find_reservation, only: %i[index show]
-  before_action :find_cookoon, only: %i[show edit update]
-  before_action :perk_spefications_collection, only: %i[new create]
+  before_action :find_cookoon, only: %i[show]
 
   def index
     filtering_params = {
@@ -34,48 +33,6 @@ class CookoonsController < ApplicationController
     end
   end
 
-  def new
-    @cookoon = Cookoon.new
-    authorize @cookoon
-  end
-
-  def create
-    @cookoon = current_user.cookoons.new(cookoon_params)
-    authorize @cookoon
-    @cookoon.status = "under_review"
-    @perks = []
-    perk_specifications.each do |perk_specification|
-      @perks << @cookoon.perks.build(perk_specification_id: perk_specification) if perk_specification.present?
-    end
-
-    if @cookoon.save
-      if current_user.stripe_account_id?
-        redirect_to home_path
-      else
-        flash[:notice] = "Vous avez presque terminé ! Pour que votre Cookoon soit visible et recevoir des paiements, \
-          vous devez connecter votre compte à notre organisme de paiement partenaire."
-        redirect_to new_stripe_account_path
-      end
-    else
-      if @cookoon.errors.messages[:user].present?
-        flash.now.alert = @cookoon.errors.messages[:user].join(" / ")
-      else
-        flash.now.alert = "Une erreur est survenue. Veuillez vérifier votre saisie et soumettre à nouveau le formulaire"
-      end
-      render :new
-    end
-  end
-
-  def edit; end
-
-  def update
-    if @cookoon.update(cookoon_params)
-      redirect_to host_dashboard_path, notice: 'Votre Cookoon a été édité !'
-    else
-      render :new
-    end
-  end
-
   private
 
   def find_cookoon
@@ -85,34 +42,5 @@ class CookoonsController < ApplicationController
 
   def find_reservation
     @reservation = Reservation.find(params[:reservation_id]).decorate
-  end
-
-  def cookoon_params
-    params.require(:cookoon).permit(
-      :name, :surface, :price, :address, :capacity, :capacity_standing, :category,
-      :digicode, :building_number, :floor_number, :door_number,
-      :wifi_network, :wifi_code, :caretaker_instructions, :citation,
-      :main_photo, :long_photo, :description, :architect_name,
-      :architect_title, :architect_url, :architect_build_year,
-      photos: []
-    )
-  end
-
-  def build_markers
-    @markers = @cookoons.map do |cookoon|
-      { lat: cookoon.latitude, lng: cookoon.longitude }
-    end
-  end
-
-  def perk_params
-    params.require(:cookoon).permit(perk_ids: [])
-  end
-
-  def perk_specifications
-    perk_params[:perk_ids]
-  end
-
-  def perk_spefications_collection
-    @perk_specifications = PerkSpecification.all.map { |perk| [("<i class='mr-1 #{perk.icon_name}'></i> #{perk.name}").html_safe, perk.id] }
   end
 end
