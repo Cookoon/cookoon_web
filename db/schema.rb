a03a1b24290a69a4453f750c67633396c2dae5a7
Alice Fabre <alice.fabre@hotmail.fr>
2020-09-30 11:22:39 +0200

    add terms_of_service to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_10_080340) do
+ActiveRecord::Schema.define(version: 2020_09_30_092107) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -264,6 +264,7 @@ ActiveRecord::Schema.define(version: 2020_09_10_080340) do
     t.integer "emailing_preferences", default: 1
     t.date "born_on"
     t.bigint "company_id"
+    t.boolean "terms_of_service"
     t.index ["company_id"], name: "index_users_on_company_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["invitation_token"], name: "index_users_on_invitation_token", unique: true
