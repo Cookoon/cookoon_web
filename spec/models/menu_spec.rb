22dc06e0e2c56c75174c2a2276e4c40532d2a02d
Alice Fabre <alice.fabre@hotmail.fr>
2020-10-13 17:55:01 +0200

    remove comments

--- spec/models/menu_spec.rb
+++ spec/models/menu_spec.rb
@@ -42,48 +42,4 @@ RSpec.describe Menu, type: :model do
     expect(menu_for_chef_with_min_price_600.computed_price_with_chef_with_margin_and_taxes_per_person(20)).to eq(Money.new 7200, 'EUR')
     expect(menu_for_chef_with_min_price_600.computed_price_with_chef_with_margin_and_taxes_per_person(50)).to eq(Money.new 7200, 'EUR')
   end
-
-  # it 'is invalid without a start date' do
-  #   reservation = build(:reservation, start_at: nil)
-  #   reservation.valid?
-  #   expect(reservation.errors[:start_at]).to include('doit être rempli(e)')
-  # end
-
-  # describe 'scopes' do
-  #   let(:classic) { create(:reservation) }
-  #   let(:paid) { create(:reservation, :paid) }
-  #   let(:two_days_ago) { create(:reservation, :created_two_days_ago) }
-  #   let(:paid_ten_days_ago) { create(:reservation, :paid, :created_ten_days_ago) }
-
-  #   describe '.dropped_before_payment' do
-  #     let(:tested_scope) { described_class.dropped_before_payment }
-
-  #     it 'returns only pending reservations created more than few hours ago' do
-  #       expect(tested_scope).to include(two_days_ago)
-  #       expect(tested_scope).to_not include(paid, classic)
-  #     end
-  #   end
-
-  #   describe '.short_notice' do
-  #     let!(:paid) { create(:reservation, :paid) }
-
-  #     it 'returns only paid reservations starting in less than few hours' do
-  #       Timecop.freeze(10.days.from_now) do
-  #         expect(described_class.short_notice).to include(paid)
-  #       end
-  #       Timecop.freeze(8.days.from_now) do
-  #         expect(described_class.short_notice).to_not include(paid)
-  #       end
-  #     end
-  #   end
-
-  #   describe '.stripe_will_not_capture' do
-  #     let(:tested_scope) { described_class.stripe_will_not_capture }
-
-  #     it 'returns only paid reservation created more than 7 days ago' do
-  #       expect(tested_scope).to include(paid_ten_days_ago)
-  #       expect(tested_scope).to_not include(paid, classic)
-  #     end
-  #   end
-  # end
 end
