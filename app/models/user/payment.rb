2896d88c66492bb23dae8d54864bd10444997412
Alice Fabre <alice.fabre@hotmail.fr>
2020-10-15 17:21:23 +0200

    add model for User::Payment

---
+++ app/models/user/payment.rb
@@ -0,0 +1,24 @@
+class User
+  class Payment < ::Payment
+    alias_method :user, :payable
+
+    def capture_inscription_payment
+      if errors.empty?
+        user.update(inscription_payment_required: false)
+      end
+    end
+
+    private
+
+    def intent_description
+      "Paiement pour l'inscription de #{user.first_name} #{user.last_name} (#{user.email})"
+    end
+
+    def intent_metadata
+      {
+        user_id: user.id,
+        inscription_price: options[:charge_amount_cents],
+      }
+    end
+  end
+end
