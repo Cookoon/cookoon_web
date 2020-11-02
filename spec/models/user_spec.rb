a83489ae5f04de62539c80cfb88632e0a9f4adee
Alice Fabre <alice.fabre@hotmail.fr>
2020-11-02 12:08:24 +0100

    add tests on validation of personal_taste & motivation presences

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,44 +1,55 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it 'is valid with a first name, last name, email and phone_number, address and job' do
-    user = build(:user, :with_job)
+  it 'is valid with a first name, last name, email and phone_number, address, job, personal_taste, motivation' do
+    user = build(:user, :with_job, :with_personal_taste, :with_motivation)
     expect(user).to be_valid
   end
 
-  # not working
+  it 'is invalid without a personal_taste' do
+    user = build(:user, :with_job, :with_motivation)
+    user.valid?
+    expect(user.errors[:personal_taste]).to include('doit être rempli(e)')
+  end
+
   it 'is invalid without a job' do
-    user = build(:user)
+    user = build(:user, :with_personal_taste, :with_motivation)
     user.valid?
     expect(user.errors[:job]).to include('doit être rempli(e)')
   end
 
+  it 'is invalid without a motivation' do
+    user = build(:user, :with_job, :with_personal_taste)
+    user.valid?
+    expect(user.errors[:motivation]).to include('doit être rempli(e)')
+  end
+
   it 'is invalid without a first name' do
-    user = build(:user, first_name: nil)
+    user = build(:user, :with_job, :with_personal_taste, :with_motivation, first_name: nil)
     user.valid?
     expect(user.errors[:first_name]).to include('doit être rempli(e)')
   end
 
   it 'is invalid without a last name' do
-    user = build(:user, last_name: nil)
+    user = build(:user, :with_job, :with_personal_taste, :with_motivation, last_name: nil)
     user.valid?
     expect(user.errors[:last_name]).to include('doit être rempli(e)')
   end
 
   it 'is invalid without a phone number' do
-    user = build(:user, phone_number: nil)
+    user = build(:user, :with_job, :with_personal_taste, :with_motivation, phone_number: nil)
     user.valid?
     expect(user.errors[:phone_number]).to include('doit être rempli(e)')
   end
 
   it 'is invalid with an invalid phone number' do
-    user = build(:user, phone_number: '06,2340')
+    user = build(:user, :with_job, :with_personal_taste, :with_motivation, phone_number: '06,2340')
     user.valid?
     expect(user.errors[:phone_number]).to include("n'est pas valide")
   end
 
   it 'returns a full name' do
-    user = build(:user)
+    user = build(:user, :with_job, :with_personal_taste, :with_motivation)
     expect(user.full_name).to eq('Aaron Sumner')
   end
 end
