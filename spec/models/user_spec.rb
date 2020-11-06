0f81529bc4536a318e0287f1e15f1b4433feb0e2
Alice Fabre <alice.fabre@hotmail.fr>
2020-11-06 14:23:26 +0100

    add test on photo // spec/models/user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it 'is valid with a first name, last name, email and phone_number, address, job, personal_taste, motivation' do
+  it 'is valid with a first name, last name, email and phone_number, address, job, personal_taste, motivation, photo' do
     user = build(:user, :with_job, :with_personal_taste, :with_motivation)
     expect(user).to be_valid
+  end
+
+  it 'is invalid without a photo' do
+    user = build(:user, :with_job, :with_personal_taste, :with_motivation)
+    user.photo = nil
+    user.valid?
+    expect(user.errors[:photo]).to include('doit être rempli(e)')
   end
 
   it 'is invalid without a personal_taste' do
