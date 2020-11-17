fd919048a15e2f824acdc6ca1704a145fa9ae440
Alice Fabre <alice.fabre@hotmail.fr>
2020-11-17 18:11:05 +0100

    change @dates_with_no_cookoon_available by @dates_with_no_cookoon_available_for current_user // app/controllers/pages_controller.rb

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,9 +5,9 @@ class PagesController < ApplicationController
   def home
     @reservation = Reservation.new.decorate
     @end_date_available = ((Date.today + Availability::SETTABLE_WEEKS_AHEAD.weeks).beginning_of_week) - 1.days
-    @dates_with_no_cookoon_available = Array.new
+    @dates_with_no_cookoon_available_for_current_user = Array.new
     (Date.today..@end_date_available).to_a.each do |date|
-      @dates_with_no_cookoon_available << date.strftime("%Y-%m-%d") if Cookoon.available_in_day(date).blank?
+      @dates_with_no_cookoon_available_for_current_user << date.strftime("%Y-%m-%d") if Cookoon.available_for(current_user).available_in_day(date).blank?
     end
   end
 
