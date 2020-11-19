b923b9e5f6c14af24c74576efed0039e998f2950
Alice Fabre <alice.fabre@hotmail.fr>
2020-11-19 13:57:09 +0100

    remove validate_cookoon_butler action / app/controllers/admin/reservations_controller.rb

--- app/controllers/admin/reservations_controller.rb
+++ app/controllers/admin/reservations_controller.rb
@@ -3,23 +3,13 @@ module Admin
     # not necessary because it is specified directly in routes
     # before_action :require_admin
     before_action :find_reservation, only: %i[show]
-    before_action :find_reservation_with_reservation_id, only: %i[validate_cookoon_butler validate_menu ask_menu_payment validate_services ask_services_payment quotation_sent quotation_accepted quotation_refused]
+    before_action :find_reservation_with_reservation_id, only: %i[validate_menu ask_menu_payment validate_services ask_services_payment quotation_sent quotation_accepted quotation_refused]
 
     def index
       @reservations = policy_scope([:admin, Reservation]).includes(:cookoon, :user, menu: [:chef], cookoon: [:user]).order(id: :desc)
     end
 
     def show
-    end
-
-    def validate_cookoon_butler
-      if @reservation.update(cookoon_butler_payment_status: "validated")
-        flash.notice = "Le décor a bien été validé"
-        redirect_to admin_reservation_path(@reservation)
-      else
-        flash.alert = "Il y a eu un problème"
-        render :show
-      end
     end
 
     def validate_menu
