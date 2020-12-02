cbc03182a0156197f1919fad188546d5c3eb6616
Alice Fabre <alice.fabre@hotmail.fr>
2020-12-02 13:18:11 +0100

    update @chefs_unavailable in chefs controller #index

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -1,5 +1,4 @@
 class ChefsController < ApplicationController
-
   before_action :find_reservation, only: %i[index show]
   before_action :find_chef, only: %i[show]
 
@@ -8,13 +7,15 @@ class ChefsController < ApplicationController
     # # policy_scope displays chef that only have active menus thanks to scope.has_active_menus in chef policy
     chefs = policy_scope(Chef).includes(:main_photo_files, :chef_perks, chef_perks: :chef_perk_specification)
     chefs_with_active_seated_menus = chefs.has_active_seated_menus
+    chefs_without_active_seated_menus = chefs.has_not_active_seated_menus
     chefs_with_active_standing_menus = chefs.has_active_standing_menus
+    chefs_without_active_standing_menus = chefs.has_not_active_standing_menus
     if @reservation.seated?
       @chefs_available = chefs_with_active_seated_menus.without_engaged_reservations_in_day(@reservation.start_at)
-      @chefs_unavailable = chefs_with_active_seated_menus.with_engaged_reservations_in_day(@reservation.start_at)
+      @chefs_unavailable = chefs_without_active_seated_menus.or(chefs_with_active_seated_menus.with_engaged_reservations_in_day(@reservation.start_at))
     elsif @reservation.standing?
-      @chefs_available = chefs_with_active_seated_menus.without_engaged_reservations_in_day(@reservation.start_at)
-      @chefs_unavailable = chefs_with_active_standing_menus.with_engaged_reservations_in_day(@reservation.start_at)
+      @chefs_available = chefs_with_active_standing_menus.without_engaged_reservations_in_day(@reservation.start_at)
+      @chefs_unavailable = chefs_without_active_standing_menus.or(chefs_with_active_standing_menus.with_engaged_reservations_in_day(@reservation.start_at))
     else
       @chefs = policy_scope(Chef).none
     end
