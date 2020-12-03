0ed269bc7e6f6a258a07f7f9ecbc22e89c705c2a
Alice Fabre <alice.fabre@hotmail.fr>
2020-12-03 23:50:39 +0100

    add regex on phone

--- app/models/amex_code.rb
+++ app/models/amex_code.rb
@@ -1,11 +1,13 @@
 class AmexCode < ApplicationRecord
+  PHONE_REGEXP = /\A(\+\d+)?([\s\-\.]?\(?\d+\)?)+\z/
+
   belongs_to :reservation, optional: true
 
   validates :code, presence: true, uniqueness: true, length: { is: 8 }
   validates :email, presence: true
   validates :first_name, presence: true, on: :update
   validates :last_name, presence: true, on: :update
-  validates :phone_number, presence: true, on: :update
+  validates :phone_number, presence: true, format: { with: PHONE_REGEXP }, on: :update
   validates :reservation, presence: true, on: :update
 
   def already_used?
