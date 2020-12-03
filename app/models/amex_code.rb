d292860425357e4e8bfc1ac0b1626e5c17bdd749
Alice Fabre <alice.fabre@hotmail.fr>
2020-12-03 23:20:11 +0100

    add validations on amex_code model

--- app/models/amex_code.rb
+++ app/models/amex_code.rb
@@ -1,10 +1,15 @@
 class AmexCode < ApplicationRecord
-  belongs_to :user, required: false
+  belongs_to :reservation, optional: true
 
-  validates :code, presence: true, uniqueness: true, length: { minimum: 12 }
-  validates :user, uniqueness: true, if: :user_is_present?
+  validates :code, presence: true, uniqueness: true, length: { is: 8 }
+  validates :email, presence: true
+  validates :first_name, presence: true, on: :update
+  validates :last_name, presence: true, on: :update
+  validates :phone_number, presence: true, on: :update
+  validates :reservation, presence: true, on: :update
 
-  def user_is_present?
-    user.present?
+  def already_used?
+    reservation.present? || first_name.present? || last_name.present? || phone_number.present?
   end
+
 end
