b9f6d5878fb0634885924ef91f2a6e315f86d7f1
Alice Fabre <alice.fabre@hotmail.fr>
2020-12-23 08:25:07 +0100

    add .start_date_available in pages & amex pages controller

--- app/controllers/un_chef_pour_vous/pages_controller.rb
+++ app/controllers/un_chef_pour_vous/pages_controller.rb
@@ -1,5 +1,6 @@
 class UnChefPourVous::PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
+  before_action :set_start_date_available, only: :home
   before_action :set_end_date_available, only: :home
   before_action :set_dates_unavailable, only: :home
 
@@ -7,16 +8,22 @@ class UnChefPourVous::PagesController < ApplicationController
     @reservation = Reservation.new(category: 'amex', people_count: 2).decorate
   end
 
+  private
+
+  def set_start_date_available
+    if Date.today < Date.new(2021, 1, 12)
+      @start_date_available = Date.new(2021, 1, 15)
+    else
+      @start_date_available = Date.today + 3.days
+    end
+  end
+
   def set_end_date_available
     @end_date_available = Date.new(2021, 6, 30)
   end
 
   def set_dates_unavailable
-    if Date.today > Date.new(2021, 1, 14)
-      @dates_unavailable = Array.new
-    else
-      @dates_unavailable = ((Date.today)..(Date.new(2021, 1, 14))).to_a.map{ |date| date.strftime("%Y-%m-%d") }
-    end
+    @dates_unavailable = Array.new
     # @dates_unavailable = Array.new
     # (Date.today..@end_date_available).to_a.each do |date|
     #   if Cookoon.amex.approved.displayable_on_index.available_in_day(date).blank? || Chef.amex.without_engaged_reservations_in_day(date).blank?
