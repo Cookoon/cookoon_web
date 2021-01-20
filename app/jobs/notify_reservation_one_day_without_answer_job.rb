3125d0865109282792cf43bd2a51b691e796c577
Alice Fabre <alice.fabre@hotmail.fr>
2021-01-20 14:38:11 +0100

    testing job mails for notify_reservation_one_day_without_answer_job and notify_user_twelve_days_after_invite_job.rb

--- app/jobs/notify_reservation_one_day_without_answer_job.rb
+++ app/jobs/notify_reservation_one_day_without_answer_job.rb
@@ -3,7 +3,7 @@ class NotifyReservationOneDayWithoutAnswerJob < ApplicationJob
 
   def perform(scheduled_time)
     scheduled_date_time = Time.zone.at(scheduled_time)
-    Reservation.needs_host_action.created_in_day_range_around(scheduled_date_time.ago(1.day)).each do |reservation|
+    Reservation.needs_host_action.created_in_day_range_around(scheduled_date_time.ago(1.hours)).each do |reservation|
       ReservationMailer.notify_awaiting_request_to_host(reservation).deliver_later
     end
   end
