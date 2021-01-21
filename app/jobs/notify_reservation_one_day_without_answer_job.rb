7c1c63c049f5b09ff243a790c1227916b090bdb7
Alice Fabre <alice.fabre@hotmail.fr>
2021-01-21 10:17:05 +0100

    update hours in simple_scheduler.staging.yml

--- app/jobs/notify_reservation_one_day_without_answer_job.rb
+++ app/jobs/notify_reservation_one_day_without_answer_job.rb
@@ -3,8 +3,9 @@ class NotifyReservationOneDayWithoutAnswerJob < ApplicationJob
 
   def perform(scheduled_time)
     scheduled_date_time = Time.zone.at(scheduled_time)
-    Reservation.needs_host_action.created_in_day_range_around(scheduled_date_time.ago(1.hours)).each do |reservation|
+    Reservation.needs_host_action.created_in_day_range_around(scheduled_date_time.ago(15.minutes)).each do |reservation|
       ReservationMailer.notify_awaiting_request_to_host(reservation).deliver_later
+      Slack::UrgencyNotifier.new(reservation: reservation).notify
     end
   end
 end
