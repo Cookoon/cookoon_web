dd385be2e5df6f64dc2bb66add2203130dc3ab33
Alice Fabre <alice.fabre@hotmail.fr>
2021-01-21 15:20:42 +0100

    correction (replace Slack::UrgencyNotifier by Slack::ReservationOneDayWithoutHostAnswerNotifier)

--- app/jobs/notify_reservation_one_day_without_host_answer_job.rb
+++ app/jobs/notify_reservation_one_day_without_host_answer_job.rb
@@ -5,7 +5,7 @@ class NotifyReservationOneDayWithoutHostAnswerJob < ApplicationJob
     scheduled_hour_time = Time.zone.at(scheduled_time)
     Reservation.needs_host_action.created_in_hour_range_around(scheduled_hour_time.ago(1.hour)).each do |reservation|
       ReservationMailer.notify_awaiting_request_to_host(reservation).deliver_later
-      Slack::UrgencyNotifier.new(reservation: reservation).notify
+      Slack::ReservationOneDayWithoutHostAnswerNotifier.new(reservation: reservation).notify
     end
   end
 end
