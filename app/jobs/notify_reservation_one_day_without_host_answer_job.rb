b3bc7c77b40550c8aacc385cedca8444ea202d18
Alice Fabre <alice.fabre@hotmail.fr>
2021-01-25 12:30:54 +0100

    change time slot for selecting reservations

--- app/jobs/notify_reservation_one_day_without_host_answer_job.rb
+++ app/jobs/notify_reservation_one_day_without_host_answer_job.rb
@@ -3,7 +3,7 @@ class NotifyReservationOneDayWithoutHostAnswerJob < ApplicationJob
 
   def perform(scheduled_time)
     scheduled_hour_time = Time.zone.at(scheduled_time)
-    Reservation.needs_host_action.created_in_hour_range_around(scheduled_hour_time.ago(1.hour)).each do |reservation|
+    Reservation.needs_host_action.created_in_hour_range_around(scheduled_hour_time.ago(25.hours)).each do |reservation|
       ReservationMailer.notify_awaiting_request_to_host(reservation).deliver_later
       # Slack::ReservationOneDayWithoutHostAnswerNotifier.new(reservation: reservation).notify
     end
